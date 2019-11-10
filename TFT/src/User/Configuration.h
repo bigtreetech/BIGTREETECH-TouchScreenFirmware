fcc67dbf5729f12dd777a1c891a7d524e15b4677
Sthopeless <6695214+Sthopeless@users.noreply.github.com>
2019-11-10 14:56:06 +0100

    reverted to default

--- TFT/src/User/Configuration.h
+++ TFT/src/User/Configuration.h
@@ -16,13 +16,13 @@
 // Marlin Mode Background & Font Color Options
 // Current color options from lcd.h: BLACK, BLUE, BROWN, BRRED, CYAN, GBLUE, GRAY, GREEN, MAGENTA, RED, WHITE, YELLOW
 #define ST7920_BKCOLOR BLACK
-#define ST7920_FNCOLOR CYAN
+#define ST7920_FNCOLOR GREEN
 
 // Text displayed at the top of the LCD in Marlin Mode. Comment out to disable.
-#define ST7920_BANNER_TEXT " "
+#define ST7920_BANNER_TEXT "LCD12864 Simulator"
 
 // Run Marlin Mode fullscreen. Not recommended for TFT24.
-#define ST7920_FULLSCREEN
+//#define ST7920_FULLSCREEN
 
 //===========================================================================
 //============================ TFT Mode Settings ============================
@@ -36,11 +36,11 @@
 #define FAN_NUM      1    // set in 1~6
 
 //                       PLA      PETG       ABS     "CUSTOM1" "CUSTOM2"
-#define PREHEAT_BED      {60,      70,       100,       55,       55}
-#define PREHEAT_HOTEND   {200,     220,      230,       200,      200}
+#define PREHEAT_BED      {55,      85,       100,       55,       55}
+#define PREHEAT_HOTEND   {205,     230,      230,       200,      200}
 //#define PREHEAT_TEXT     {"PLA",  "PETG",   "ABS",     "T2:",    "T3:"}
 
-#define HEAT_MAX_TEMP    {120,    275,       275,       275,       275,       275,       275}    //max temperature can be set
+#define HEAT_MAX_TEMP    {150,    275,       275,       275,       275,       275,       275}    //max temperature can be set
 #define HEAT_SIGN_ID     {"B:",   "T0:",     "T1:",     "T2:",     "T3:",     "T4:",     "T5:"}
 #define HEAT_DISPLAY_ID  {"Bed",  "T0",      "T1",      "T2",      "T3",      "T4",      "T5"}
 #define HEAT_CMD         {"M140", "M104 T0", "M104 T1", "M104 T2", "M104 T3", "M104 T4", "M104 T5" };
@@ -67,7 +67,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS 235
 #define Y_MAX_POS 235
-#define Z_MAX_POS 270
+#define Z_MAX_POS 250
 
 // Specify a pause position as { X, Y, Z_raise }
 #define NOZZLE_PAUSE_RETRACT_LENGTH 15   // (mm)
@@ -108,11 +108,11 @@
 
 // Enable alternative Move Menu Buttons Layout for easy
 // update the icons from alternate icon folder
-#define ALTERNATIVE_MOVE_MENU
+//#define ALTERNATIVE_MOVE_MENU
 
 // Enable Unified Move Menu
 // Move, Home, Extrude, ABL at one Place and bring Gcode Menu and
-#define UNIFIED_MENU
+//#define UNIFIED_MENU
 
 // SD support
 #define ONBOARD_SD_SUPPORT
