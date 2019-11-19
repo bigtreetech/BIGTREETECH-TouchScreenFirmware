ce88d79fb3a522fe4c40658c01a6be1770c9c44d
LJL19970813 <1823531583@qq.com>
2019-11-19 15:37:27 +0800

    add TFT28_V3_0

--- TFT/src/User/variants.h
+++ TFT/src/User/variants.h
@@ -28,7 +28,7 @@
   #define F_CPUM 72
   #define STM32F10X_CL
   #include "stm32f10x.h"
-#elif defined(TFT35_V3_0)
+#elif defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define VECT_TAB_FLASH 0x08008000
   #define HSE_VALUE ((uint32_t)8000000) //8Mhz
   #define F_CPUM 120
@@ -41,7 +41,7 @@
   #define RM68042
   #define STM32_HAS_FSMC
   #define LCD_DATA_16BIT 0
-#elif defined(TFT35_V1_2) || defined(TFT35_V2_0) || defined(TFT35_V3_0)
+#elif defined(TFT35_V1_2) || defined(TFT35_V2_0) || defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define ILI9488
   #define STM32_HAS_FSMC
   #define LCD_DATA_16BIT 1
@@ -57,7 +57,7 @@
 //LCD Backlight pin (PWM can adjust brightness)
 #if defined(TFT24_V1_1)
   #define LCD_LED_PIN   PA8
-#elif defined(TFT35_V3_0)
+#elif defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define LCD_LED_PIN   PD12
 #endif
 
@@ -67,7 +67,7 @@
   #define SERIAL_PORT   _USART1  //default usart port
   #define SERIAL_PORT_2 _USART2
   #define SERIAL_PORT_3 _USART3
-#elif defined(TFT35_V3_0)
+#elif defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define SERIAL_PORT   _USART2  //default usart port
   #define SERIAL_PORT_2 _USART1
   #define SERIAL_PORT_3 _USART3
@@ -94,7 +94,7 @@
   #define XPT2046_MISO  PB4
   #define XPT2046_MOSI  PB5
   #define XPT2046_TPEN  PC1
-#elif defined(TFT35_V3_0)
+#elif defined(TFT35_V3_0) || defined(TFT28_V3_0) 
   #define XPT2046_CS    PE6
   #define XPT2046_SCK   PE5
   #define XPT2046_MISO  PE4
@@ -111,7 +111,7 @@
   #define SD_HIGH_SPEED 0
   #define SD_SPI        _SPI2
   #define SPI2_CS_PIN   PB12
-#elif defined(TFT24_V1_1) || defined(TFT35_V3_0)
+#elif defined(TFT24_V1_1) || defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define SD_SPI_SUPPORT
   #define SD_LOW_SPEED  7
   #define SD_HIGH_SPEED 1
@@ -128,7 +128,7 @@
   #define SD_CD_PIN     PA13
 #elif defined(TFT35_V2_0)
   #define SD_CD_PIN     PC6
-#elif defined(TFT35_V3_0)
+#elif defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define SD_CD_PIN     PC4
 #endif
 
@@ -141,14 +141,14 @@
   #define W25Qxx_SPEED  0
   #define W25Qxx_SPI    _SPI3
   #define SPI3_CS_PIN   PD2
-#elif defined(TFT35_V3_0)
+#elif defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define W25Qxx_SPEED  1
   #define W25Qxx_SPI    _SPI3
   #define SPI3_CS_PIN   PB6
 #endif
 
 //ST7920 Simulator SPI pins
-#if defined(TFT24_V1_1) || defined(TFT35_V3_0)
+#if defined(TFT24_V1_1) || defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define ST7920_SPI    _SPI2
 #endif
 
@@ -157,7 +157,7 @@
   #define BUZZER_PIN    PA14
 #elif defined(TFT35_V2_0)
   #define BUZZER_PIN    PB2
-#elif defined(TFT35_V3_0)
+#elif defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define BUZZER_PIN    PD13
 #endif
 
@@ -167,7 +167,7 @@
   #define LCD_ENCA_PIN  PB0
   #define LCD_ENCB_PIN  PB1
   #define LCD_BTN_PIN   PB2
-#elif defined(TFT35_V3_0)
+#elif defined(TFT35_V3_0) || defined(TFT28_V3_0)
   //PA8:ENC-A PC9:ENC-B PC8:BTN 
   #define LCD_ENCA_PIN  PA8
   #define LCD_ENCB_PIN  PC9
@@ -176,7 +176,7 @@
 #define LCD_ENCODER_SUPPORT (defined(LCD_ENCA_PIN) && defined(LCD_ENCB_PIN) && defined(LCD_BTN_PIN))
 
 //U disk support
-#if defined(TFT24_V1_1) || defined(TFT35_V3_0)
+#if defined(TFT24_V1_1) || defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define U_DISK_SUPPROT
   #define USE_USB_OTG_FS
 #endif
@@ -185,7 +185,7 @@
 #if defined(TFT35_V2_0)
   #define PS_ON_PIN      PD12
   #define FIL_RUNOUT_PIN PD11
-#elif defined(TFT35_V3_0)
+#elif defined(TFT35_V3_0) || defined(TFT28_V3_0)
   #define PS_ON_PIN      PA12
   #define FIL_RUNOUT_PIN PA15
 #endif
@@ -195,7 +195,7 @@
   #define DISABLE_JTAG    //free JTAG(PB3/PB4) for SPI3
 #elif defined(TFT24_V1_1)
   #define DISABLE_DEBUG   //
-#elif defined(TFT35_V3_0)
+#elif defined(TFT35_V3_0) || defined(TFT28_V3_0)
   //stm32f207 needn't this
 #endif
 
@@ -212,7 +212,7 @@
   #define TITLE_END_Y 60
   
   #define selecticonw 95
-#elif defined(TFT28_V1_0) || defined(TFT24_V1_1)
+#elif defined(TFT28_V1_0) || defined(TFT24_V1_1) || defined(TFT28_V3_0)
   #define LCD_WIDTH	  320
   #define LCD_HEIGHT	240
   
@@ -226,9 +226,15 @@
   #define selecticonw 70
 #endif
 
+#if defined(TFT28_V3_0) || defined(TFT35_V3_0)
+  #define LED_color_PIN PC7
+#endif
+
+
+
 #if defined(TFT35_V1_0) || defined(TFT35_V1_1) || defined(TFT35_V1_2)|| defined(TFT35_V2_0) || defined(TFT35_V3_0)
   #define ROOT_DIR "TFT35"
-#elif defined(TFT28_V1_0)
+#elif defined(TFT28_V1_0) || defined(TFT28_V3_0)
   #define ROOT_DIR "TFT28"
 #elif defined(TFT24_V1_1)
   #define ROOT_DIR "TFT24"
@@ -250,6 +256,9 @@
     #define HARDWARE_VERSION "TFT35_V2.0"
   #elif defined(TFT35_V3_0)
     #define HARDWARE_VERSION "TFT35_V3.0"
+  #elif defined(TFT28_V3_0)
+    #define HARDWARE_VERSION "TFT28_V3.0"
+
   #endif
 #endif
 
