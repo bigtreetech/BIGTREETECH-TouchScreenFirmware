54712800457d4d63eba1f49f879c0d8014e93b57
chzj333 <872543189@qq.com>
2019-11-30 20:55:41 +0800

    parameter1

--- TFT/src/User/Hal/xpt2046.c
+++ TFT/src/User/Hal/xpt2046.c
@@ -1,8 +1,8 @@
 #include "xpt2046.h"
 #include "GPIO_Init.h"
 #include "includes.h"
-/***************************************** XPT2046 SPI ģʽ�ײ���ֲ�Ľӿ�********************************************/
-//XPT2046 SPI��� - ʹ��ģ��SPI
+/***************************************** XPT2046 SPI 模式底层移植的接口********************************************/
+//XPT2046 SPI相关 - 使用模拟SPI
 _SW_SPI xpt2046;
 
 //Ƭѡ
@@ -11,13 +11,13 @@ void XPT2046_CS_Set(u8 level)
   SW_SPI_CS_Set(&xpt2046, level);
 }
 
-//��д����
+//读写函数
 u8 XPT2046_ReadWriteByte(u8 TxData)
 {		
   return SW_SPI_Read_Write(&xpt2046, TxData);			    
 }
 
-//XPT2046 SPI�ͱ��жϳ�ʼ��
+//XPT2046 SPI和笔中断初始化
 void XPT2046_Init(void)
 {
   //PA15-TPEN
@@ -32,14 +32,14 @@ void XPT2046_Init(void)
   XPT2046_CS_Set(1);
 }
 
-//�����ж�
+//读笔中断
 u8 XPT2046_Read_Pen(void)
 {
   return GPIO_GetLevel(XPT2046_TPEN);
 }
 /******************************************************************************************************************/
 
-//��ȡ XPT2046 ת���õ�ADֵ
+//读取 XPT2046 转化好的AD值
 u16 XPT2046_Read_AD(u8 CMD)
 {
   u16 ADNum;
@@ -48,14 +48,14 @@ u16 XPT2046_Read_AD(u8 CMD)
   XPT2046_ReadWriteByte(CMD);
   ADNum=XPT2046_ReadWriteByte(0xff);
   ADNum= ((ADNum)<<8) | XPT2046_ReadWriteByte(0xff);
-  ADNum >>= 4;         //XPT2046����ֻ��12bits,��������λ
+  ADNum >>= 4;         //XPT2046数据只有12bits,舍弃低四位
 
   XPT2046_CS_Set(1);
   return ADNum;
 }
 
-#define READ_TIMES 5 	//��ȡ����
-#define LOST_VAL 1	  	//����ֵ
+#define READ_TIMES 5 	//读取次数
+#define LOST_VAL 1	  	//丢弃值
 u16 XPT2046_Average_AD(u8 CMD)
 {
   u16 i, j;
@@ -63,11 +63,11 @@ u16 XPT2046_Average_AD(u8 CMD)
   u16 sum=0;
   u16 temp;
   for(i=0; i<READ_TIMES; i++) buf[i] = XPT2046_Read_AD(CMD);		 		    
-  for(i=0; i<READ_TIMES-1; i++)//����
+  for(i=0; i<READ_TIMES-1; i++)//排序
   {
     for(j=i+1; j<READ_TIMES; j++)
     {
-      if(buf[i] > buf[j]) //��������
+      if(buf[i] > buf[j]) //升序排列
       {
         temp = buf[i];
         buf[i] = buf[j];
@@ -82,7 +82,7 @@ u16 XPT2046_Average_AD(u8 CMD)
 } 
 
 
-#define ERR_RANGE 50 //��Χ 
+#define ERR_RANGE 50 //误差范围 
 u16 XPT2046_Repeated_Compare_AD(u8 CMD) 
 {
   u16 ad1, ad2;
@@ -90,7 +90,7 @@ u16 XPT2046_Repeated_Compare_AD(u8 CMD)
   ad2 = XPT2046_Average_AD(CMD);
 
   if((ad2 <= ad1 && ad1 < ad2 + ERR_RANGE) 
-  || (ad1 <= ad2 && ad2 < ad1 + ERR_RANGE)) //ǰ���������С�� ERR_RANGE
+  || (ad1 <= ad2 && ad2 < ad1 + ERR_RANGE)) //前后两次误差小于 ERR_RANGE
   {
     return (ad1+ad2)/2;
   }
