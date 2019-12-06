2ca82c81454d54089de6ef91b5caee80e4149831
bigtreetech <bigtreetech@gmail.com>
2019-12-06 11:57:18 +0800

    add miniUPS support in power failed

--- TFT/src/User/Configuration.h
+++ TFT/src/User/Configuration.h
@@ -150,6 +150,10 @@
  * This function is suitable for Delta Printer.
  */
 //#define HOME_BEFORE_PLR
+// 
+#define BTT_MINI_UPS // Backup power / UPS to move the Z axis steppers on power loss
+#define POWER_LOSS_ZRAISE 10 // (mm) Z axis raise on resume (on power loss with UPS)
+
 
 // Prevent extrusion if the temperature is below set temperature
 #define PREVENT_COLD_EXTRUSION_MINTEMP 170
