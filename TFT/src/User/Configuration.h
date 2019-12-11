ce8e0a897b28b350f5acfad4913d317a9f036d80
melufre <melufre@users.noreply.github.com>
2019-12-11 21:03:10 +0100

    configure with E12 bed size

--- TFT/src/User/Configuration.h
+++ TFT/src/User/Configuration.h
@@ -67,9 +67,9 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS 235
-#define Y_MAX_POS 235
-#define Z_MAX_POS 250
+#define X_MAX_POS 300
+#define Y_MAX_POS 300
+#define Z_MAX_POS 400
 
 // Specify a pause position as { X, Y, Z_raise }
 #define NOZZLE_PAUSE_RETRACT_LENGTH 15   // (mm)
