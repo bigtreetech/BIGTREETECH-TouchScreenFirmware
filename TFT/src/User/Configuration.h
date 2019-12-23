798743bbbebdf0ed55e0f72277716e19bf5414e8
BigTreeTech <38851044+bigtreetech@users.noreply.github.com>
2019-12-23 15:57:33 +0800

    Czech translations for new messages (#255)
    * Small updates
    
    * Czech translations for new messages
    
    * Added INVERT_ZAXIS for printers with bed moving up/down
    
    Co-authored-by: 3darena <54456380+3darena@users.noreply.github.com>

--- TFT/src/User/Configuration.h
+++ TFT/src/User/Configuration.h
@@ -121,6 +121,8 @@
 //Invert the Y Axis move Direction
 //#define INVERT_YAXIS
 
+//Invert the Z Axis move Direction
+//#define INVERT_ZAXIS
 
 // Enable Unified Move Menu
 // Move, Home, Extrude, ABL at one Place and bring Gcode Menu on Home Menu
