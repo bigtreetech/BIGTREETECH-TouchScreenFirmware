856d29184cb7c530a61de7108bd2521e503fe1d9
guruathwal <gurmeet.athwal@gmail.com>
2020-01-26 17:53:05 +0530

    fix typo and minor bug

--- TFT/src/User/API/Language/language_en.h
+++ TFT/src/User/API/Language/language_en.h
@@ -97,7 +97,7 @@
     #define EN_5_PERCENT            "5%"
     #define EN_10_PERCENT           "10%"
     #define EN_READY                "Ready"
-    #define EN_BUSY                 "Busy processing ,please wait..."
+    #define EN_BUSY                 "Busy processing, please wait..."
     #define EN_UNCONNECTED          "No printer attached!"
     #define EN_DISCONNECT_INFO      "Now you can control the printer by your computer!"
     #define EN_LOADING              "Loading..."
