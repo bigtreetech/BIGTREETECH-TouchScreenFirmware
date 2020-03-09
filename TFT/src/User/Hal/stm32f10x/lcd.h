6f5c850ad85d56c7fa8ff8d6fd858c7d9d4d1210
Robby Candra <robbycandra.mail@gmail.com>
2020-03-09 09:24:59 +0700

    Clean Up Color Definition (#310)

--- TFT/src/User/Hal/stm32f10x/lcd.h
+++ TFT/src/User/Hal/stm32f10x/lcd.h
@@ -11,15 +11,15 @@
     volatile uint16_t LCD_REG;
     volatile uint16_t LCD_RAM;
   } LCD_TypeDef;
-                                
-  #define LCD_BASE        ((uint32_t)(0x60000000 | 0x00FFFFFE))  	//1111 1111 1111 1111 1111 1110	
+
+  #define LCD_BASE        ((uint32_t)(0x60000000 | 0x00FFFFFE))  	//1111 1111 1111 1111 1111 1110
   #define LCD             ((LCD_TypeDef *) LCD_BASE)
 
   #define LCD_WR_REG(regval) do{ LCD->LCD_REG = regval; }while(0)
   #define LCD_WR_DATA(data)  do{ LCD->LCD_RAM = data; }while(0)
-  
+
 #else
-  
+
   /* 配置控制线
   * PB6   :LCD-RD
   * PB7   :LCD-WR
@@ -27,26 +27,26 @@
   * PB8   :LCD-RS   LCD-RS发送的数据代表是命令还是显示数据
   */
   #define	LCD_CS_SET  GPIOB->BSRR=1<<9    //片选端口    PB9
-  #define	LCD_RS_SET	GPIOB->BSRR=1<<8    //数据/命令 	PB8	   
+  #define	LCD_RS_SET	GPIOB->BSRR=1<<8    //数据/命令 	PB8
   #define	LCD_WR_SET	GPIOB->BSRR=1<<7    //写数据			PB7
   #define	LCD_RD_SET	GPIOB->BSRR=1<<6    //读数据			PB6
-                      
+
   #define	LCD_CS_CLR  GPIOB->BRR=1<<9     //片选端口  	PB9
-  #define	LCD_RS_CLR	GPIOB->BRR=1<<8     //数据/命令		PB8	   
+  #define	LCD_RS_CLR	GPIOB->BRR=1<<8     //数据/命令		PB8
   #define	LCD_WR_CLR	GPIOB->BRR=1<<7     //写数据			PB7
-  #define	LCD_RD_CLR	GPIOB->BRR=1<<6     //读数据			PB6   
+  #define	LCD_RD_CLR	GPIOB->BRR=1<<6     //读数据			PB6
 
   //PB0~15,作为数据线
   #define DATAOUT(x) do{GPIOC->ODR = x;}while(0) //数据输出
-  #define DATAIN()     GPIOC->IDR                //数据输入	
+  #define DATAIN()     GPIOC->IDR                //数据输入
 
   void LCD_WR_REG(uint16_t data);
   void LCD_WR_DATA(uint16_t data);
-  
+
 #endif
 
+// Color Definition
 
-//
 #define WHITE                 0xFFFF
 #define BLACK                 0x0000
 #define BLUE                  0x001F
