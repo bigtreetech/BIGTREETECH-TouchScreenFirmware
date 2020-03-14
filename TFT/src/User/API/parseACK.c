8e7fa695d30b72abcc2b7ea46d744ded4fe63a29
bigtreetech <bigtreetech@gmail.com>
2020-03-14 17:26:42 +0800

    improve smart filament detect, auto get E steps by M92 after start up

--- TFT/src/User/API/parseACK.c
+++ TFT/src/User/API/parseACK.c
@@ -80,10 +80,15 @@ void ackPopupInfo(const char *info)
   popupReminder((u8* )info, (u8 *)dmaL2Cache + ack_index);
 }
 
+bool dmaL1NotEmpty(uint8_t port)
+{
+  return dmaL1Data[port].rIndex != dmaL1Data[port].wIndex;
+}
+
 void syncL2CacheFromL1(uint8_t port)
 {
   uint16_t i = 0;
-  for (i = 0; dmaL1Data[port].rIndex != dmaL1Data[port].wIndex; i++)
+  for (i = 0; dmaL1NotEmpty(port) && dmaL2Cache[i-1] != '\n'; i++)
   {
     dmaL2Cache[i] = dmaL1Data[port].cache[dmaL1Data[port].rIndex];
     dmaL1Data[port].rIndex = (dmaL1Data[port].rIndex + 1) % DMA_TRANS_LEN;
@@ -96,212 +101,203 @@ void parseACK(void)
   bool avoid_terminal = false;
   if(infoHost.rx_ok[SERIAL_PORT] != true) return; //not get response data
 
-  syncL2CacheFromL1(SERIAL_PORT);
-  infoHost.rx_ok[SERIAL_PORT] = false;
+  while(dmaL1NotEmpty(SERIAL_PORT))
+  {
+    syncL2CacheFromL1(SERIAL_PORT);
+    infoHost.rx_ok[SERIAL_PORT] = false;
 
-  if(infoHost.connected == false) //not connected to Marlin
-  {
-    if((!ack_seen("T:") && !ack_seen("T0:")) || !ack_seen("ok"))  goto parse_end;  //the first response should be such as "T:25/50 ok\n"
-    updateLastHeatCheckTime();
-    infoHost.connected = true;
-    #ifdef AUTO_SAVE_LOAD_LEVELING_VALUE
-      storeCmd("M420 S1\n");
-    #endif
-  }
-
-  // GCode command response
-  if(requestCommandInfo.inWaitResponse && ack_seen(requestCommandInfo.startMagic))
-  {
-    requestCommandInfo.inResponse = true;
-    requestCommandInfo.inWaitResponse = false;
-  }
-  if(requestCommandInfo.inResponse)
-  {
-    if(strlen(requestCommandInfo.cmd_rev_buf)+strlen(dmaL2Cache) < CMD_MAX_REV)
+    if(infoHost.connected == false) //not connected to Marlin
     {
-      strcat(requestCommandInfo.cmd_rev_buf, dmaL2Cache);
+      if((!ack_seen("T:") && !ack_seen("T0:")) || !ack_seen("ok"))  goto parse_end;  //the first response should be such as "T:25/50 ok\n"
+        updateLastHeatCheckTime();
+        infoHost.connected = true;
+        #ifdef AUTO_SAVE_LOAD_LEVELING_VALUE
+          storeCmd("M420 S1\n");
+        #endif
+        storeCmd("M92\n"); // Get steps/mm for smart filament sensor after start up
+    }
 
-      if(ack_seen(requestCommandInfo.errorMagic ))
+    // Gcode command response
+    if(requestCommandInfo.inWaitResponse && ack_seen(requestCommandInfo.startMagic))
+    {
+      requestCommandInfo.inResponse = true;
+      requestCommandInfo.inWaitResponse = false;
+    }
+    if(requestCommandInfo.inResponse)
+    {
+      if(strlen(requestCommandInfo.cmd_rev_buf)+strlen(dmaL2Cache) < CMD_MAX_REV)
+      {
+        strcat(requestCommandInfo.cmd_rev_buf, dmaL2Cache);
+
+        if(ack_seen(requestCommandInfo.errorMagic ))
+        {
+          requestCommandInfo.done = true;
+          requestCommandInfo.inResponse = false;
+          requestCommandInfo.inError = true;
+        }
+        else if(ack_seen(requestCommandInfo.stopMagic ))
+        {
+          requestCommandInfo.done = true;
+          requestCommandInfo.inResponse = false;
+        }
+      }
+      else
       {
         requestCommandInfo.done = true;
         requestCommandInfo.inResponse = false;
-        requestCommandInfo.inError = true;
+        ackPopupInfo(errormagic);
       }
-      else if(ack_seen(requestCommandInfo.stopMagic ))
-      {
-        requestCommandInfo.done = true;
-        requestCommandInfo.inResponse = false;
-      }
+      infoHost.wait = false;
+      goto parse_end;
     }
-    else
-    {
-      requestCommandInfo.done = true;
-      requestCommandInfo.inResponse = false;
-      ackPopupInfo(errormagic);
-    }
-    infoHost.wait = false;
-    goto parse_end;
-  }
-  // end
+    // end
 
-  if(ack_cmp("ok\n"))
-  {
-    infoHost.wait = false;
-  }
-  else
-  {
-    if(ack_seen("ok"))
+    if(ack_cmp("ok\n"))
     {
       infoHost.wait = false;
     }
-    if(ack_seen("X:"))
+    else
     {
-      storegantry(0, ack_value());
-      //storeCmd("M118 %d\n", ack_value());
-      if (ack_seen("Y:"))
+      if(ack_seen("ok"))
       {
-        storegantry(1, ack_value());
+        infoHost.wait = false;
+      }
+      if(ack_seen("X:"))
+      {
+        storegantry(0, ack_value());
         //storeCmd("M118 %d\n", ack_value());
-        if (ack_seen("Z:"))
+        if (ack_seen("Y:"))
         {
+          storegantry(1, ack_value());
           //storeCmd("M118 %d\n", ack_value());
-          storegantry(2, ack_value());
+          if (ack_seen("Z:"))
+          {
+            //storeCmd("M118 %d\n", ack_value());
+            storegantry(2, ack_value());
+          }
         }
       }
-    }
-    else if(ack_seen("T:") || ack_seen("T0:"))
-    {
-      heatSetCurrentTemp(heatGetCurrentToolNozzle(), ack_value()+0.5);
-      heatSyncTargetTemp(heatGetCurrentToolNozzle(), ack_second_value()+0.5);
-      for(TOOL i = BED; i < HEATER_NUM; i++)
+      else if(ack_seen("T:") || ack_seen("T0:"))
       {
-        if(ack_seen(toolID[i]))
+        heatSetCurrentTemp(heatGetCurrentToolNozzle(), ack_value()+0.5);
+        heatSyncTargetTemp(heatGetCurrentToolNozzle(), ack_second_value()+0.5);
+        for(TOOL i = BED; i < HEATER_NUM; i++)
         {
-          heatSetCurrentTemp(i, ack_value()+0.5);
-          heatSyncTargetTemp(i, ack_second_value()+0.5);
+          if(ack_seen(toolID[i]))
+          {
+            heatSetCurrentTemp(i, ack_value()+0.5);
+            heatSyncTargetTemp(i, ack_second_value()+0.5);
+          }
+        }
+        avoid_terminal = infoSettings.terminalACK;
+        updateLastHeatCheckTime();
+      }
+      else if(ack_seen("B:"))
+      {
+        heatSetCurrentTemp(BED, ack_value()+0.5);
+        heatSyncTargetTemp(BED, ack_second_value()+0.5);
+        avoid_terminal = infoSettings.terminalACK;
+        updateLastHeatCheckTime();
+      }
+      else if(ack_seen("Mean:"))
+      {
+        popupReminder((u8* )"Repeatability Test", (u8 *)dmaL2Cache + ack_index-5);
+      }
+      else if(ack_seen("Probe Offset"))
+      {
+        if(ack_seen("Z"))
+        {
+          setCurrentOffset(ack_value());
         }
       }
-      avoid_terminal = infoSettings.terminalACK;
-      updateLastHeatCheckTime();
-    }
-    else if(ack_seen("B:"))
-    {
-      heatSetCurrentTemp(BED, ack_value()+0.5);
-      heatSyncTargetTemp(BED, ack_second_value()+0.5);
-      avoid_terminal = infoSettings.terminalACK;
-      updateLastHeatCheckTime();
-    }
-    else if(ack_seen("Mean:"))
-    {
-      popupReminder((u8* )"Repeatability Test", (u8 *)dmaL2Cache + ack_index-5);
-      //popupReminder((u8* )"Standard Deviation", (u8 *)&infoCmd.queue[infoCmd.index_r].gcode[5]);
-    }
-    else if(ack_seen("Probe Offset"))
-    {
-      if(ack_seen("Z"))
+      else if(ack_seen("Count E:")) // parse actual position, response of "M114"
       {
-        setCurrentOffset(ack_value());
+        coordinateSetAxisActualSteps(E_AXIS, ack_value());
       }
-    }
-    else if(ack_seen("Count E:")) // parse actual position, response of "M114"
-    {
-      coordinateSetAxisActualSteps(E_AXIS, ack_value());
-    }
-    else if(ack_seen(echomagic) && ack_seen(busymagic) && ack_seen("processing"))
-    {
-      busyIndicator(STATUS_BUSY);
-    }
-    else if(ack_seen(echomagic) && ack_seen(busymagic) && ack_seen("paused for user"))
-    {
-      goto parse_end;
-    }
-    //parse and store stepper driver current values
-    else if(ack_seen("X driver current: "))
-    {
-      setParameterCurrent(0,ack_value());
-
-      if(ack_seen("Y driver current: "))
-      setParameterCurrent(1,ack_value());
-
-      if(ack_seen("Z driver current: "))
-      setParameterCurrent(2,ack_value());
-
-      if(ack_seen("E driver current: "))
-      setParameterCurrent(3,ack_value());
-    }
-    //parse and store stepper steps/mm values
-    else if(ack_seen("M92 X"))
-    {
-      setParameterSteps(0,ack_value());
-
-      if(ack_seen("Y"))
-      setParameterSteps(1,ack_value());
-
-      if(ack_seen("Z"))
-      setParameterSteps(2,ack_value());
-
-      if(ack_seen("E"))
-      setParameterSteps(3,ack_value());
-    }
-
-#ifdef ONBOARD_SD_SUPPORT
-    else if(ack_seen(bsdnoprintingmagic) && infoMenu.menu[infoMenu.cur] == menuPrinting)
-    {
-      infoHost.printing = false;
-      completePrinting();
-    }
-    else if(ack_seen(bsdprintingmagic))
-    {
-      if(infoMenu.menu[infoMenu.cur] != menuPrinting && !infoHost.printing) {
-        infoMenu.menu[++infoMenu.cur] = menuPrinting;
-        infoHost.printing=true;
-      }
-      // Parsing printing data
-      // Example: SD printing byte 123/12345
-      char *ptr;
-      u32 position = strtol(strstr(dmaL2Cache, "byte ")+5, &ptr, 10);
-      setPrintCur(position);
-//      powerFailedCache(position);
-    }
-#endif
-    else if(ack_seen(errormagic))
-    {
-      ackPopupInfo(errormagic);
-    }
-    else if(ack_seen(echomagic))
-    {
-      for(u8 i = 0; i < COUNT(ignoreEcho); i++)
+      else if(ack_seen(echomagic) && ack_seen(busymagic) && ack_seen("processing"))
       {
-        if(strstr(dmaL2Cache, ignoreEcho[i])) goto parse_end;
+        busyIndicator(STATUS_BUSY);
       }
-      ackPopupInfo(echomagic);
-    }
-  }
-  if (ack_seen(" F0:"))
-  {
-      fanSetSpeed(0, ack_value());
-  }
-
-parse_end:
-  if(ack_cur_src != SERIAL_PORT)
-  {
-    Serial_Puts(ack_cur_src, dmaL2Cache);
-  }
-  else if (!ack_seen("ok"))
-  {
-    // make sure we pass on spontaneous messages to all connected ports (since these can come unrequested)
-    for (int port = 0; port < _USART_CNT; port++)
-    {
-      if (port != SERIAL_PORT && portSeen[port])
+      else if(ack_seen(echomagic) && ack_seen(busymagic) && ack_seen("paused for user"))
       {
-        // pass on this one to anyone else who might be listening
-        Serial_Puts(port, dmaL2Cache);
+        goto parse_end;
+      }
+      //parse and store stepper driver current values
+      else if(ack_seen("X driver current: "))
+      {
+        setParameterCurrent(X_AXIS, ack_value());
+        if(ack_seen("Y driver current: ")) setParameterCurrent(Y_AXIS, ack_value());
+        if(ack_seen("Z driver current: ")) setParameterCurrent(Z_AXIS, ack_value());
+        if(ack_seen("E driver current: ")) setParameterCurrent(E_AXIS, ack_value());
+      }
+      //parse and store stepper steps/mm values
+      else if(ack_seen("M92 X"))
+      {
+        setParameterSteps(X_AXIS, ack_value());
+        if(ack_seen("Y")) setParameterSteps(Y_AXIS, ack_value());
+        if(ack_seen("Z")) setParameterSteps(Z_AXIS, ack_value());
+        if(ack_seen("E")) setParameterSteps(E_AXIS, ack_value());
+      }
+
+  #ifdef ONBOARD_SD_SUPPORT
+      else if(ack_seen(bsdnoprintingmagic) && infoMenu.menu[infoMenu.cur] == menuPrinting)
+      {
+        infoHost.printing = false;
+        completePrinting();
+      }
+      else if(ack_seen(bsdprintingmagic))
+      {
+        if(infoMenu.menu[infoMenu.cur] != menuPrinting && !infoHost.printing) {
+          infoMenu.menu[++infoMenu.cur] = menuPrinting;
+          infoHost.printing=true;
+        }
+        // Parsing printing data
+        // Example: SD printing byte 123/12345
+        char *ptr;
+        u32 position = strtol(strstr(dmaL2Cache, "byte ")+5, &ptr, 10);
+        setPrintCur(position);
+  //      powerFailedCache(position);
+      }
+  #endif
+      else if(ack_seen(errormagic))
+      {
+        ackPopupInfo(errormagic);
+      }
+      else if(ack_seen(echomagic))
+      {
+        for(u8 i = 0; i < COUNT(ignoreEcho); i++)
+        {
+          if(strstr(dmaL2Cache, ignoreEcho[i])) goto parse_end;
+        }
+        ackPopupInfo(echomagic);
       }
     }
-  }
+    if (ack_seen(" F0:"))
+    {
+        fanSetSpeed(0, ack_value());
+    }
 
-  if (avoid_terminal != true){
-    sendGcodeTerminalCache(dmaL2Cache, TERMINAL_ACK);
+  parse_end:
+    if(ack_cur_src != SERIAL_PORT)
+    {
+      Serial_Puts(ack_cur_src, dmaL2Cache);
+    }
+    else if (!ack_seen("ok"))
+    {
+      // make sure we pass on spontaneous messages to all connected ports (since these can come unrequested)
+      for (int port = 0; port < _USART_CNT; port++)
+      {
+        if (port != SERIAL_PORT && portSeen[port])
+        {
+          // pass on this one to anyone else who might be listening
+          Serial_Puts(port, dmaL2Cache);
+        }
+      }
+    }
+
+    if (avoid_terminal != true){
+      sendGcodeTerminalCache(dmaL2Cache, TERMINAL_ACK);
+    }
   }
 }
 
