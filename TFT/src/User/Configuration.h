6b13472a505592f2a31d046496973e8225c75d05
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2020-03-27 04:56:11 -0700

    Disable Start & End Gcode by Default (#461)

--- TFT/src/User/Configuration.h
+++ TFT/src/User/Configuration.h
@@ -310,7 +310,7 @@
  *
  * G-code that runs after a print starts, ends, or canceled.
  *
- * Disable in SETTINGS -> FEATURE menu.
+ * Enable Start & End G-code in SETTINGS -> FEATURE menu.
  */
 // Start G-code - run this G-code before starting print
 #define PRINT_START_GCODE "G28\nG29\nG1 Z20\n" // Home all, run ABL routine, raise Z 20 mm
