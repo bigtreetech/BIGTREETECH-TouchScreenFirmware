1affd5ad1280b52254bc072fe79850329fe24c8b
Msq001 <760675063@qq.com>
2020-04-01 17:33:05 +0800

    Bugfix cleanup lib (#502)
    * clean Hal libraries.
    * now the SPI and UART pins can be set in "pin_TFTxx.h" file or "Configuration.h" file

--- TFT/src/User/includes.h
+++ TFT/src/User/includes.h
@@ -22,7 +22,7 @@
 #include "Language.h"
 #include "utf8_decode.h"
 
-#include "usart.h"
+#include "uart.h"
 #include "Serial.h"
 #include "spi.h"
 #include "sw_spi.h"
@@ -103,7 +103,7 @@ extern MENU infoMenu;
 typedef struct
 {
   bool wait;       //Whether wait for Marlin's response
-  bool rx_ok[_USART_CNT]; //Whether receive Marlin's response or get Gcode by other UART(ESP3D/OctoPrint)
+  bool rx_ok[_UART_CNT]; //Whether receive Marlin's response or get Gcode by other UART(ESP3D/OctoPrint)
   bool connected;  //Whether have connected to Marlin
   bool printing;   //Whether the host is busy in printing execution. ( USB serial printing and GCODE print from onboard)
 }HOST;
