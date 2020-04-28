4ce3ad1a01493eaa19b77b97a6ec3606042fc850
guruathwal <gurmeet.athwal@gmail.com>
2020-04-28 15:52:28 +0530

    change PRINT_CANCEL_GCODE behaviour
    Enable PRINT_CANCEL_GCODE by default and change PRINT_CANCEL_GCODE behaviour to be sent only when printing from TFT's SD card and USB flash disk to avoid duplicate Cancel G-codes when printing from Onboard SD card.

--- TFT/src/User/Menu/Printing.c
+++ TFT/src/User/Menu/Printing.c
@@ -693,15 +693,14 @@ void abortPrinting(void)
 
     case TFT_UDISK:
     case TFT_SD:
+      if (infoSettings.send_cancel_gcode == 1)
+        mustStoreCmd(PRINT_CANCEL_GCODE);
+
       clearCmdQueue();
       break;
   }
 
   heatClearIsWaiting();
-
-  if(infoSettings.send_cancel_gcode == 1){
-    mustStoreCmd(PRINT_CANCEL_GCODE);
-  }
 
   endPrinting();
   exitPrinting();
