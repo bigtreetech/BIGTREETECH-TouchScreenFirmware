d70b8d4c2eae2ac48a3159e821dd40d58014942e
Luc <8822552+luc-github@users.noreply.github.com>
2020-05-03 15:29:54 +0200

    Broadcast temperatures to Serial 2

--- TFT/src/User/API/parseACK.c
+++ TFT/src/User/API/parseACK.c
@@ -410,7 +410,7 @@ void parseACK(void)
     {
       Serial_Puts(ack_cur_src, dmaL2Cache);
     }
-    else if (!ack_seen("ok"))
+    else if (!ack_seen("ok") || ack_seen("T:") || ack_seen("T0:"))
     {
       // make sure we pass on spontaneous messages to all connected ports (since these can come unrequested)
       for (int port = 0; port < _UART_CNT; port++)
