14916fab251095c7a31edf28aa22433aba0b1a41
guruathwal <gurmeet.athwal@gmail.com>
2020-05-20 13:18:16 +0530

    Update config.h

--- TFT/src/User/API/config.h
+++ TFT/src/User/API/config.h
@@ -125,7 +125,7 @@
 #define CONFIG_CANCEL_GCODE     "cancel_gcode:"
 
 //-----------------------------Limits
-#define MAX_SIZE_LIMIT            1000         // machine size over this will not be parsed.
+#define MAX_SIZE_LIMIT            2000         // machine size over this will not be parsed.
 #define MAX_EXT_SPEED_LIMIT       5000         // Extruder speed over this will not pe parsed.
 #define MAX_TOOL_TEMP             300          // extruder temp over this will not pe parsed.
 #define MAX_BED_TEMP              200          // bed temp over this will not pe parsed.
@@ -136,7 +136,7 @@
 #define MAX_RETRACT_LIMIT         20           // retract length over this will not be parsed.
 #define MAX_FAN_SPEED             255          // fan speed (PWM) over this will not be parsed.
 
-#define MIN_SIZE_LIMIT            -1000     // machine size less than this will not be parsed.
+#define MIN_SIZE_LIMIT            -2000     // machine size less than this will not be parsed.
 #define NAME_MIN_LENGTH           3         // minimum name length
 #define GCODE_MIN_LENGTH          3         // gcode length less than this will not pe parsed.
 #define MIN_POS_LIMIT             0         // position value less than this will not be parsed.
