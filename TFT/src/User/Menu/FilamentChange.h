7a29021c76b089068125eceeb4b61e13ac25a175
Gustavo Cevallos <gstavocevallos@gmail.com>
2020-06-24 18:21:15 -0500

    Remove unexpose function

--- TFT/src/User/Menu/FilamentChange.h
+++ TFT/src/User/Menu/FilamentChange.h
@@ -1,7 +1,6 @@
 #ifndef _FILAMENTCHANGE_H_
 #define _FILAMENTCHANGE_H_
 
-extern const char* toofilamentChange_tool_changel_change[];
 extern const char* filamentChangeDisplayID[];
 
 void menuFilamentChange(void);
