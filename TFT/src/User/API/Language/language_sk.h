334785fe5bb08e7621461ebef5eaa098bf386559
NicoletoAerials <67210427+NicoletoAerials@users.noreply.github.com>
2020-07-28 20:09:51 -0500

    Update language_sk.h

--- TFT/src/User/API/Language/language_sk.h
+++ TFT/src/User/API/Language/language_sk.h
@@ -101,6 +101,8 @@
     #define SK_1_MM                   "1mm"
     #define SK_5_MM                   "5mm"
     #define SK_10_MM                  "10mm"
+    #define SK_100_MM                 "100mm"
+    #define SK_400_MM                 "400mm"
     #define SK_1_PERCENT              "1%"
     #define SK_5_PERCENT              "5%"
     #define SK_10_PERCENT             "10%"
