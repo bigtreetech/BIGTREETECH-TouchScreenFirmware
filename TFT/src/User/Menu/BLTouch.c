4e8ced958a336ca40e2f1677982dbe26f71083f1
bryan065 <30362590+bryan065@users.noreply.github.com>
2020-08-05 01:58:35 -0400

    Add BLTouch Reset option to menu (#903)

--- TFT/src/User/Menu/BLTouch.c
+++ TFT/src/User/Menu/BLTouch.c
@@ -5,11 +5,11 @@ const MENUITEMS BLTouchItems = {
 // title
 LABEL_BLTOUCH,
 // icon                        label
- {{ICON_BLTOUCH_DEPLOY,        LABEL_BLTOUCH_DEPLOY},
-  {ICON_BLTOUCH_STOW,          LABEL_BLTOUCH_STOW},
+ {{ICON_BLTOUCH_RESET,         LABEL_RESET},
   {ICON_BLTOUCH_TEST,          LABEL_BLTOUCH_TEST},
+  {ICON_BLTOUCH_DEPLOY,        LABEL_BLTOUCH_DEPLOY},
+  {ICON_BLTOUCH_STOW,          LABEL_BLTOUCH_STOW},
   {ICON_BLTOUCH_REPEAT,        LABEL_BLTOUCH_REPEAT},
-  {ICON_BACKGROUND,            LABEL_BACKGROUND},
   {ICON_BACKGROUND,            LABEL_BACKGROUND},
   {ICON_BACKGROUND,            LABEL_BACKGROUND},
   {ICON_BACK,                  LABEL_BACK},}
@@ -25,15 +25,18 @@ void menuBLTouch(void)
     switch(key_num)
     {
       case KEY_ICON_0:
-        storeCmd("M280 P0 S10\n");
+        storeCmd("M280 P0 S160\n");
         break;
       case KEY_ICON_1:
-        storeCmd("M280 P0 S90\n");
-        break;
-      case KEY_ICON_2:
         storeCmd("M280 P0 S120\n");
         break;
+      case KEY_ICON_2:
+        storeCmd("M280 P0 S10\n");
+        break;
       case KEY_ICON_3:
+        storeCmd("M280 P0 S90\n");
+        break;
+      case KEY_ICON_4:
         storeCmd("G28\n");
         storeCmd("M48\n");
         break;
