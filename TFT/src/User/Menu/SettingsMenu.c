f0a97b20b27cd8f4a188454368250800bf7eb6d3
bryan065 <30362590+bryan065@users.noreply.github.com>
2020-08-06 23:46:20 -0400

    Updated parameters menu
    Added EEPROM save button & auto firmware retract (M209)

--- TFT/src/User/Menu/SettingsMenu.c
+++ TFT/src/User/Menu/SettingsMenu.c
@@ -80,6 +80,7 @@ void menuSettings(void)
         break;
 
       case KEY_ICON_1:
+        mustStoreCmd("M503 S0\n");
         infoMenu.menu[++infoMenu.cur] = menuMachineSettings;
         break;
 
