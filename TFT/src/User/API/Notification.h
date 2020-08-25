b4ebd65c2983fd65ed34b9281a9762561e882eca
guruathwal <gurmeet.athwal@gmail.com>
2020-08-25 11:56:41 +0530

    Update Notification.h

--- TFT/src/User/API/Notification.h
+++ TFT/src/User/API/Notification.h
@@ -1,13 +1,18 @@
+#ifndef _NOTIFICATION_H_
+#define _NOTIFICATION_H_
+
 #include "menu.h"
 
 typedef struct
 {
   DIALOG_TYPE style;
-  uint8_t     isNew;
-  char        text[TOAST_MSG_LENGTH];
+  uint8_t isNew;
+  char text[TOAST_MSG_LENGTH];
 }TOAST;
 
 bool toastRunning(void);
 void addToast(DIALOG_TYPE style, char * text);
 void drawToast(bool redraw);
 void loopToast(void);
+
+#endif
