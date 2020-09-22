be74ca4cd4e7c8796cd28aa6cbeb8750e804dcae
digant73 <diguardo@tiscali.it>
2020-09-22 21:26:00 +0200

    created API for Babystep and Probe Offset used by new MBL and next Mesh editor

---
+++ TFT/src/User/API/BabystepControl.h
@@ -0,0 +1,30 @@
+#ifndef _BABYSTEP_CONTROL_H_
+#define _BABYSTEP_CONTROL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Reset only babystep value to default value */
+float babystepReset(void);
+
+/* Get current babystep value */
+float babystepGetValue(void);
+
+/* Reset babystep value to default value */
+float babystepResetValue(void);
+
+// Decrease babystep value
+float babystepDecreaseValue(float unit);
+
+// Increase babystep value
+float babystepIncreaseValue(float unit);
+
+// Update babystep value by encoder
+float babystepUpdateValueByEncoder(float unit);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
