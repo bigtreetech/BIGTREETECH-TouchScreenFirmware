581595f1e35f92431d34dbf2ea6e503b5675a817
petervw <info@petrossystems.nl>
2020-10-25 14:49:38 +0100

    new start, added new files
    The way languages are set is changed. Made a new branch for it

---
+++ TFT/src/User/API/neopixel_sequential.h
@@ -0,0 +1,14 @@
+#ifndef _NEOPIXEL_SEQUENTIAL_H_
+#define _NEOPIXEL_SEQUENTIAL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void setNeopixelSequentialColor(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
