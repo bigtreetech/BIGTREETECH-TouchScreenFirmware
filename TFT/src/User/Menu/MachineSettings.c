34cb863aaf24096c24808a16c52cbf1fa97092d6
digant73 <diguardo@tiscali.it>
2020-10-26 15:14:37 +0100

    fixed bug #1207

--- TFT/src/User/Menu/MachineSettings.c
+++ TFT/src/User/Menu/MachineSettings.c
@@ -212,22 +212,28 @@ void menuEepromSettings(void)
       case KEY_ICON_0:
         // save to EEPROM
         if (infoMachineSettings.EEPROM == 1)
+        {
           setDialogText(eepromSettingsItems.title.index, LABEL_EEPROM_SAVE_INFO, LABEL_CONFIRM, LABEL_CANCEL);
-        showDialog(DIALOG_TYPE_QUESTION, saveEepromSettings, NULL, NULL);
+          showDialog(DIALOG_TYPE_QUESTION, saveEepromSettings, NULL, NULL);
+        }
         break;
 
       case KEY_ICON_1:
         // restore from EEPROM
         if (infoMachineSettings.EEPROM == 1)
+        {
           setDialogText(eepromSettingsItems.title.index, LABEL_EEPROM_RESTORE_INFO, LABEL_CONFIRM, LABEL_CANCEL);
           showDialog(DIALOG_TYPE_QUESTION, restoreEepromSettings, NULL, NULL);
+        }
         break;
 
       case KEY_ICON_2:
         // reset EEPROM
         if (infoMachineSettings.EEPROM == 1)
+        {
           setDialogText(eepromSettingsItems.title.index, LABEL_EEPROM_RESET_INFO, LABEL_CONFIRM, LABEL_CANCEL);
           showDialog(DIALOG_TYPE_QUESTION, resetEepromSettings, NULL, NULL);
+        }
         break;
 
       case KEY_ICON_7:
