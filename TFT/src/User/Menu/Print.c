ee680c358e4d8c3e204d3da790c685854e6b4e46
radek8 <46979052+radek8@users.noreply.github.com>
2020-11-15 20:11:24 +0100

    Config.ini

--- TFT/src/User/Menu/Print.c
+++ TFT/src/User/Menu/Print.c
@@ -406,7 +406,7 @@ void menuPrint(void)
             list_mode = true; //force list mode in Onboard sd card
             infoFile.source = BOARD_SD;
             infoMenu.menu[++infoMenu.cur] = menuPrintFromSource;   //TODO: fix here,  onboard sd card PLR feature
-            storeCmd("M155 S1\n");
+            storeCmd("M155 S3\n");
             goto selectEnd;
           }
           break;
