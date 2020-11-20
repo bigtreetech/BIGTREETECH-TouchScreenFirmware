b671a49c943d028efd623f6b4fea3d9e8217939d
mehmetsutas <46458775+mehmetsutas@users.noreply.github.com>
2020-11-20 15:41:37 +0300

    Update MainPage.c

--- TFT/src/User/Menu/MainPage.c
+++ TFT/src/User/Menu/MainPage.c
@@ -36,7 +36,7 @@ void unifiedMenu(void)
         break;
 
       case KEY_ICON_2:
-        infoMenu.menu[++infoMenu.cur] = menuExtrude;
+        infoMenu.menu[++infoMenu.cur] = menuPreheat;
         break;
 
       case KEY_ICON_3:
