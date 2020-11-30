da3b87e3d20d777a9572d1bdea9346a6e7bac8f0
kisslorand <50251547+kisslorand@users.noreply.github.com>
2020-11-30 03:17:24 +0200

    Mkstft28.bin filename output for mks 28 v1 0 board (#1301)

--- buildroot/scripts/short_out_filename.py
+++ buildroot/scripts/short_out_filename.py
@@ -6,7 +6,10 @@ def make_short_file(source, target, env):
     buildDir = env.subst('$BUILD_DIR')
     build_flags = env.ParseFlags(env['BUILD_FLAGS'])
     flags = {k: v for (k, v) in build_flags.get("CPPDEFINES")}
-    filename = flags.get("HARDWARE") + "." + flags.get("SOFTWARE_VERSION")
+    if flags.get("HARDWARE") == "MKS_28_V1_0":
+        filename = "MKSTFT28"
+    else:
+        filename = flags.get("HARDWARE") + "." + flags.get("SOFTWARE_VERSION")
     filename_short = flags.get("HARDWARE_SHORT") + flags.get("SOFTWARE_VERSION_SHORT")
     # do some actions
     shutil.copyfile(buildDir+"/"+filename+".bin", buildDir+"/"+filename_short+".new")
