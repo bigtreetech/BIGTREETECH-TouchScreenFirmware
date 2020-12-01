4f86c3a3d995abc0c2c43c0e2f4f44837461d4c8
Kiss Lorand <kisslorand666@yahoo.com>
2020-12-02 01:35:52 +0200

    Onbord SD issue v2

--- TFT/src/User/Menu/SendGcode.c
+++ TFT/src/User/Menu/SendGcode.c
@@ -6,7 +6,7 @@
   #define TERMINAL_MAX_CHAR ((LCD_WIDTH / BYTE_WIDTH) * (LCD_HEIGHT / BYTE_HEIGHT) * 6)  // for 48k RAM
 #endif*/
 #if ((defined(MKS_28_V1_0)) || (defined(MKS_32_V1_4) && !defined(MKS_32_V1_4_NOBL)))
-  #define TERMINAL_MAX_CHAR  5000 // for 64k RAM 
+  #define TERMINAL_MAX_CHAR  5000 // for 64k RAM
 #else
   #define TERMINAL_MAX_CHAR  3700 // for 48k RAM
 #endif
