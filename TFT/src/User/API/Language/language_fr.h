8283043dc8fbf87af8bedca2aeeb473183e0e7dd
mehmetsutas <46458775+mehmetsutas@users.noreply.github.com>
2020-12-05 18:16:29 +0300

    Update language_fr.h

--- TFT/src/User/API/Language/language_fr.h
+++ TFT/src/User/API/Language/language_fr.h
@@ -86,8 +86,8 @@
     #define STRING_RESUME                 "Reprendre"
     #define STRING_LOAD                   "Charger"
     #define STRING_UNLOAD                 "Décharger"
-    #define STRING_LOAD_UNLOAD            "Load/Unload"  // needs translation
-    #define STRING_LOAD_UNLOAD_SHORT      "(Un)Load"  // needs translation
+    #define STRING_LOAD_UNLOAD            "Charger / Décharger"  // needs translation
+    #define STRING_LOAD_UNLOAD_SHORT      "Décharger"  // needs translation
     #define STRING_SLOW_SPEED             "Lent"
     #define STRING_NORMAL_SPEED           "Normal"
     #define STRING_FAST_SPEED             "Rapide"
@@ -279,9 +279,9 @@
     #define STRING_MESH_EDITOR            "Maillage"
     #define STRING_MESH_TUNER             "Edition du maillage"
     #define STRING_CASE_LIGHT             "Case light"
-    #define STRING_LOAD_STARTED           "Filament load in progress.\nNo further actions possible\nuntil it's done.\nPlease stand by."  // translation needed
-    #define STRING_UNLOAD_STARTED         "Filament unload in progress.\nNo further actions possible\nuntil it's done.\nPlease stand by."  // translation needed
-    #define STRING_HEATERS_ON             "Some heaters are still ON.\nDo you want to stop them?"  // translation needed
+    #define STRING_LOAD_STARTED           "Chargement du filament en cours, aucune action possible avant la fin du processus." 
+    #define STRING_UNLOAD_STARTED         "Déchargement du filament en cours, aucune action possible avant la fin du processus.."
+    #define STRING_HEATERS_ON             "Certains éléments de chauffe sont encore activés, voulez vous les arreter ?" 
     #define STRING_PRINT_TOTAL_TIME       "Print complete.\nPrint Time: %02u:%02u:%02u \n"  // translation needed
     #define STRING_PRINT_FILAMENT_USED    "Filament Used: %u meters"  // translation needed
 
