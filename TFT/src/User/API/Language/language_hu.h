6de3eb4c71530150b5ffc820eff9325a0e30770e
Zs.Antal <45710979+AntoszHUN@users.noreply.github.com>
2020-12-08 08:27:24 +0100

    Update Hungarian language. (#1345)

--- TFT/src/User/API/Language/language_hu.h
+++ TFT/src/User/API/Language/language_hu.h
@@ -185,7 +185,7 @@
     #define STRING_EMERGENCYSTOP          "VészSTOP"
     #define STRING_TOUCH_TO_EXIT          "Érintsd meg a kilépéshez."
     #define STRING_MAINMENU               "Főmenü"
-    #define STRING_WAIT_TEMP_SHUT_DOWN    "Várd meg a fej\nvisszahűlését.%d℃" // Várj, míg a fej a megadott ℃ alá hűl.
+    #define STRING_WAIT_TEMP_SHUT_DOWN    "Várd meg a fej\nvisszahűlését.%d℃"
     #define STRING_FORCE_SHUT_DOWN        "Kényszerítés"
     #define STRING_SHUTTING_DOWN          "Leállítás..."
     #define STRING_PARAMETER_SETTING      "Tényezők"
@@ -279,8 +279,8 @@
     #define STRING_MESH_EDITOR            "Háló Szerkesztő"
     #define STRING_MESH_TUNER             "Háló Hangoló"
     #define STRING_CASE_LIGHT             "Gépház Világítás"
-    #define STRING_LOAD_STARTED           "A betöltési művelet folyamatban van,\nnincs lehtőség beavakozásra,\nmíg véget nem ér.\nVárjál!"
-    #define STRING_UNLOAD_STARTED         "A kiadási eljárás folyamatban van,\nnincs lehtőség beavakozásra,\nmíg véget nem ér.\nVárjál!"
-    #define STRING_HEATERS_ON             "Fűtés(ek) bekapcsolva maradt(ak).\nKikapcsolod az összes fűtést?"  // translation needed
+    #define STRING_LOAD_STARTED           "A filament betöltése folyamatban van,\nnem lehetséges további művelet,\nmíg véget nem ér.\nKérlek várj!"
+    #define STRING_UNLOAD_STARTED         "A filament kiadása folyamatban van,\nnem lehetséges további művelet,\nmíg véget nem ér.\nKérlek várj!"
+    #define STRING_HEATERS_ON             "Néhány fűtőelem még üzemel.\nKikapcsolod a fűtőelemeket?"
 
 #endif
