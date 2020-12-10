3a6a5dc5c2d065319cff6053d169ab0cfa3ef4ef
mehmetsutas <46458775+mehmetsutas@users.noreply.github.com>
2020-12-10 15:15:08 +0300

    Update Printing.c

--- TFT/src/User/API/Printing.c
+++ TFT/src/User/API/Printing.c
@@ -255,6 +255,7 @@ void endPrinting(void)
   switch (infoFile.source)
   {
     case BOARD_SD:
+      request_M27(0);
       break;
 
     case TFT_UDISK:
@@ -263,7 +264,6 @@ void endPrinting(void)
       break;
   }
   infoPrinting.printing = infoPrinting.pause = false;
-  request_M27(0);
   powerFailedClose();
   powerFailedDelete();
   if((infoFile.source != BOARD_SD) && (infoSettings.send_end_gcode == 1))
