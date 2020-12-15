fcdddc178eaac4a4d7b934877d1a124fd92e7886
BsCmOD <m.boscolo@inwind.it>
2020-12-15 22:44:36 +0000

    Update/enhaced italian language

--- TFT/src/User/API/Language/language_it.h
+++ TFT/src/User/API/Language/language_it.h
@@ -282,10 +282,10 @@
     #define STRING_LOAD_STARTED           "Carico filamento in corso...\nNon sono possibili altre\nazioni fino al termine.\nAttendere prego."
     #define STRING_UNLOAD_STARTED         "Scarico filamento in corso...\nNon sono possibili altre\nazioni fino al termine.\nAttendere prego."
     #define STRING_HEATERS_ON             "Alcuni riscaldatori sono ancora accesi\nVuoi spegnerli?"
-    #define STRING_PRINT_SUMMARY_POPUP    "Sommario"
+    #define STRING_PRINT_SUMMARY_POPUP    "Sommario di fine stampa"
     #define STRING_PRINT_SUMMARY          "Durata totale: %02u:%02u:%02u\nFilamento usato: %3.2f metri"
     #define STRING_PRINT_COMPLETE         "Stampa completata"
-    #define STRING_EXT_TEMPLOW            "Hotend temperature is below\nminimum temperature (%d℃)."
-    #define STRING_COLD_EXT               "Cold extrusion prevented"
+    #define STRING_EXT_TEMPLOW            "La temperatura dell'hotend\nè al di sotto della temperatura\nminima (%d℃)."
+    #define STRING_COLD_EXT               "Prevenzione dell'estrusione a freddo"
 
 #endif
