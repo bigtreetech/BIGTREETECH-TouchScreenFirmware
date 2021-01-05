c2389253b7edf8ab79181dab6a2a3e3ed2ee4c70
Msq001 <760675063@qq.com>
2021-01-05 09:36:27 +0800

    ScreenShot feature for document (#1418)

---
+++ TFT/src/User/API/ScreenShot.h
@@ -0,0 +1,18 @@
+#ifndef _SCREEN_SHOT_H_
+#define _SCREEN_SHOT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "variants.h"
+#include "stdbool.h"
+
+bool screenShotBMP(char *bmp);
+void loopScreenShot(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
