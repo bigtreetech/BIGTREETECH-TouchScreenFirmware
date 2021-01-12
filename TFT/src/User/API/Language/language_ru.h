4ae57ff61e581467307a128cd5afbf46e6307e21
Cyril <guilouz6@hotmail.com>
2021-01-12 09:53:49 +0100

    Add Z Steppers Auto-Alignment (G34) (#1483)

--- TFT/src/User/API/Language/language_ru.h
+++ TFT/src/User/API/Language/language_ru.h
@@ -295,5 +295,6 @@
     #define STRING_EXT_TEMPLOW            "Температура сопла ниже\nминимальной (%d℃)."
     #define STRING_COLD_EXT               "Экструзия холодным\nсоплом запрещена"
     #define STRING_HEAT_HOTEND            "Heat hotend to %d℃?"
+    #define STRING_Z_ALIGN                "Z Align"
 
 #endif
