a01b21cf7a64d802429f8b7e557ef7dee285cf8e
radek8 <46979052+radek8@users.noreply.github.com>
2021-01-18 22:18:30 +0100

    Notification_icons.
    For a printer, 2 hotends add an icon with the temperature of the second hotend to the notification bar.
    For a printer with 1 or 3 or more, it remains unchanged

--- TFT/src/User/Menu/Parametersetting.c
+++ TFT/src/User/Menu/Parametersetting.c
@@ -366,13 +366,23 @@ int16_t drawTemperatureStatus(void)
   int16_t x_offset = LCD_WIDTH - 10;
   if (!temperatureStatusValid()) return x_offset;
 
-  uint8_t tmpHeater[3]; // chamber, bed, hotend
-  uint16_t tmpIcon[3];
+  uint8_t tmpHeater[4]; // 1-2hotend, bed, chamber hotend
+  uint16_t tmpIcon[4];
   uint8_t tmpIndex = 0;
   if (infoSettings.hotend_count)
   { // global hotend
+    if (infoSettings.hotend_count == 2)  // dual hotend
+    { 
+    tmpIcon[tmpIndex] = ICON_GLOBAL_NOZZLE;
+    tmpHeater[tmpIndex++] = NOZZLE0;
+    tmpIcon[tmpIndex] = ICON_GLOBAL_NOZZLE;
+    tmpHeater[tmpIndex++] = NOZZLE1;
+    }
+    else // singl or mixing hotend
+    { 
     tmpIcon[tmpIndex] = ICON_GLOBAL_NOZZLE;
     tmpHeater[tmpIndex++] = heatGetCurrentHotend();
+    }
   }
   if (infoSettings.bed_en)
   { // global bed
