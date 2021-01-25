87c06b765a9e22e063104927c0b3fd438fe6565b
Msq001 <alansayyeah@gmail.com>
2021-01-25 18:43:53 +0800

    update framework

--- buildroot/scripts/stm32f2xx_0x8000_iap.py
+++ buildroot/scripts/stm32f2xx_0x8000_iap.py
@@ -5,6 +5,8 @@ Import("env")
 # Relocate firmware from 0x08000000 to 0x08008000
 custom_ld_script = os.path.abspath("buildroot/ldscripts/stm32f2xx_0x8000_iap.ld")
 
+env.Replace(LDSCRIPT_PATH=custom_ld_script)
+print(custom_ld_script)
 for i, flag in enumerate(env["LINKFLAGS"]):
     if "-Wl,-T" in flag:
         env["LINKFLAGS"][i] = "-Wl,-T" + custom_ld_script
