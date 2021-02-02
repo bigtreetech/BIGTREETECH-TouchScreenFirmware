f76de9c244f30f74303612efb6696a57eedb2d0a
kisslorand <kisslorand666@yahoo.com>
2021-02-02 11:58:07 +0200

    Back

--- TFT/src/User/API/Language/language_hu.h
+++ TFT/src/User/API/Language/language_hu.h
@@ -1,7 +1,7 @@
 #ifndef _LANGUAGE_HU_H_
 #define _LANGUAGE_HU_H_
 
-    // config.ini Parameter Settings / Feature Settings
+    // config.ini Paraméterbeállítások / Funkcióbeállítások
     #define STRING_STATUS_SCREEN          "Állapot képernyő aktív"
     #define STRING_BAUDRATE               "Kapcsolat"
     #define STRING_LANGUAGE               "Magyar"
@@ -47,7 +47,7 @@
     #define STRING_END_GCODE_ENABLED      "Befejező G-Kód"
     #define STRING_CANCEL_GCODE_ENABLED   "Leállító G-Kód"
 
-    // Machine Parameter Settings
+    // Gépparaméter-beállítások
     #define STRING_STEPS_SETTING          "Lépés beállítás"
     #define STRING_MAXFEEDRATE            "Max előtolási sebesség"
     #define STRING_MAXACCELERATION        "Max gyorsulás"
@@ -77,7 +77,7 @@
     #define STRING_SWAP_RECOVER_FEEDRATE  "Csere előtolás jav."
     #define STRING_MBL_OFFSET             "MBL Eltolás"
 
-    // Save / Load
+    // Mentés / Betöltés
     #define STRING_SAVE                   "Mentés"
     #define STRING_RESTORE                "Betöltés"
     #define STRING_RESET                  "Újratölt"
@@ -90,7 +90,7 @@
     #define STRING_SETTINGS_RESET_INFO    "Minden beállítás vissza\nfog állni alapértelmezett\nértékre. Folytatod?"
     #define STRING_SETTINGS_RESET_DONE    "A beállítások visszaállítása\nsikeresen megtörtént.\nAz aktiváláshoz indítsd\nújra az eszközt."
 
-    // Navigation Buttons
+    // Navigációs gombok
     #define STRING_PAGE_UP                "Fel"
     #define STRING_PAGE_DOWN              "Le"
     #define STRING_UP                     "Fel"
@@ -98,7 +98,7 @@
     #define STRING_NEXT                   "Tovább"
     #define STRING_BACK                   "Vissza"
 
-    // Value Buttons
+    // Érték gombok
     #define STRING_INC                    "Növel"
     #define STRING_DEC                    "Csökkent"
     #define STRING_LOAD                   "Betöltés"
@@ -117,7 +117,7 @@
     #define STRING_CLEAR                  "Törlés"
     #define STRING_DEFAULT                "Alapért."
 
-    // Action Buttons
+    // Művelet gombok
     #define STRING_START                  "Kezdés"
     #define STRING_STOP                   "Állj"
     #define STRING_PAUSE                  "Szünet"
@@ -131,21 +131,21 @@
     #define STRING_PREHEAT_BOTH           "Mindkettő"
     #define STRING_COOLDOWN               "Lehűtés"
 
-    // Dialog Buttons
+    // Párbeszédgombok
     #define STRING_CONFIRM                "\u088E Oké"
     #define STRING_CANCEL                 "\u088F Mégse"
     #define STRING_WARNING                "Figyelem!"
     #define STRING_CONTINUE               "Folytatás"
     #define STRING_CONFIRMATION           "Biztos vagy benne?"
 
-    // Process Status
+    // Folyamat állapota
     #define STRING_STATUS                 "Státusz"
     #define STRING_READY                  "Kész"
     #define STRING_BUSY                   "Feldolgozás alatt, kérlek várj..."
     #define STRING_LOADING                "Betöltés..."
     #define STRING_UNCONNECTED            "Nincs nyomtató csatlakoztatva!"
 
-    // Process Info
+    // Folyamat infó
     #define STRING_INFO                   "Infó"
     #define STRING_INVALID_VALUE          "Nincs érvényes érték/s megadva"
     #define STRING_TIMEOUT_REACHED        "Időkorlát elérve!"
@@ -157,7 +157,7 @@
     #define STRING_PROCESS_COMPLETED      "Folyamat befejezve!"
     #define STRING_PROCESS_ABORTED        "Folyamat megszakítva!"
 
-    // TFT SD, U_DISK, Onboard SD, Filament Runout Process Commands / Status / Info
+    // TFT SD, U_DISK, Alaplap SD, Szál Kifutás Folyamatparancsok / Státusz / Infó
     #define STRING_TFTSD                  "TFT-SD"
     #define STRING_READ_TFTSD_ERROR       "TFT-SD olvasási hiba!"
     #define STRING_TFTSD_INSERTED         "SD-Kártya behelyezve!"
@@ -170,7 +170,7 @@
     #define STRING_READ_ONBOARDSD_ERROR   "Alaplap-SD olvasási hiba!"
     #define STRING_FILAMENT_RUNOUT        "Szál kifutás!"
 
-    // Steppers, Print, Probe Process Commands / Status / Info
+    // Léptetők, Nyomtatás, Szonda folyamat parancsai / Státusz / Infó
     #define STRING_DISABLE_STEPPERS       "Mot.tilt"
     #define STRING_XY_UNLOCK              "Felold XY"
 
@@ -184,7 +184,7 @@
     #define STRING_STOW                   "Elhelyezés"
     #define STRING_REPEAT                 "Ismétlés"
 
-    // Printer Tools
+    // Nyomtatóeszközök
     #define STRING_NOZZLE                 "Fej"
     #define STRING_BED                    "Ágy"
     #define STRING_CHAMBER                "Kamra"
@@ -193,7 +193,7 @@
     #define STRING_BLTOUCH                "BLTouch"
     #define STRING_TOUCHMI                "TouchMi"
 
-    // Values
+    // Értékek
     #define STRING_1_DEGREE               "1℃"
     #define STRING_5_DEGREE               "5℃"
     #define STRING_10_DEGREE              "10℃"
@@ -218,7 +218,7 @@
     #define STRING_120_SECONDS            "2 Perc."
     #define STRING_300_SECONDS            "5 Perc."
 
-    // Colors
+    // Színek
     #define STRING_WHITE                  "Fehér"
     #define STRING_BLACK                  "Fekete"
     #define STRING_BLUE                   "Kék"
@@ -238,7 +238,7 @@
     #define STRING_DARKGREEN              "Sötétzöld"
     #define STRING_DARKGRAY               "Sötétszürke"
 
-    // Menus
+    // Menük
     #define STRING_HEAT                   "Fűtés"
     #define STRING_MOVE                   "Mozgás"
     #define STRING_HOME                   "Végállás"
