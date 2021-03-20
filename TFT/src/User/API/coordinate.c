5228ac3b81eac07fc8ed46128cb6fbca8a306896
Cal1sto <47449942+Cal1sto@users.noreply.github.com>
2021-03-20 13:57:34 +0100

    Update coordinate.c
    2 spaces for indentation, not 4 ---> done

--- TFT/src/User/API/coordinate.c
+++ TFT/src/User/API/coordinate.c
@@ -79,13 +79,13 @@ float coordinateGetAxisTarget(AXIS axis)
 // Set level corner position the measured Z offset from probe, see in ABL.c menu refreshLevelCornerValue(MENUITEMS levelItems) and value get from parseACK.c
 void SetLevelCornerPosition(int point,float position)
 {
-    levelCornerPosition[point] = position;
+  levelCornerPosition[point] = position;
 }
 
 // Get level corner position the measured Z offset from probe, see in ABL.c menu refreshLevelCornerValue(MENUITEMS levelItems) and value get from parseACK.c
 float GetLevelCornerPosition(int point)
 {
-    return levelCornerPosition[point];
+  return levelCornerPosition[point];
 }
 
 uint32_t coordinateGetFeedRate(void)
