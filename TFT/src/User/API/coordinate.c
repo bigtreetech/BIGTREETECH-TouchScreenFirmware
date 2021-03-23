97d9e3bc58258c18b16e1716747c2e9e0a5da04c
Cal1sto <Cal1sto@free.fr>
2021-03-23 11:36:11 +0100

    Update coordinate.c

--- TFT/src/User/API/coordinate.c
+++ TFT/src/User/API/coordinate.c
@@ -7,7 +7,7 @@ const char axis_id[TOTAL_AXIS] = {'X', 'Y', 'Z', 'E'};
 static COORDINATE targetPosition = {{0.0f, 0.0f, 0.0f, 0.0f}, 3000};
 static COORDINATE curPosition = {{0.0f, 0.0f, 0.0f, 0.0f}, 3000};
 
-static float levelCornerPosition[5]; // buffer current z value measured, position 1, position 2, position 3, position 4
+static float levelCornerPosition[6]; // buffer current z value measured = {Flag (new value), position 1, position 2, position 3, position 4, probe accuracy(M48)}
 /**
  * Obtained from "M114 E" instead of "M114", Because the coordinates of "M114" are not real-time coordinates.
  * It may be replaced by "M114 R".
