0a7b4b5b2252b263567fab6d6c748f32d9aa8f91
Cal1sto <47449942+Cal1sto@users.noreply.github.com>
2021-03-24 11:43:49 +0100

    Update menu.c
    Move the text on icone more into the icone area for 24x24 bitmap font

--- TFT/src/User/API/menu.c
+++ TFT/src/User/API/menu.c
@@ -642,12 +642,12 @@ void showTextOnIcon(uint8_t index, uint8_t Levelindex, const LIVE_INFO * liveico
   GUI_SetColor(ORANGE);
   GUI_SetTextMode(GUI_TEXTMODE_TRANS);
   GUI_POINT loc;
-  loc.x = liveicon->lines[Levelindex].pos.x + curRect[index].x0 + 2;
-  loc.y = liveicon->lines[Levelindex].pos.y + curRect[index].y0 - 2;
+  loc.x = liveicon->lines[Levelindex].pos.x + curRect[index].x0 + 1;
+  loc.y = liveicon->lines[Levelindex].pos.y + curRect[index].y0 - 8;
   setLargeFont(VAL_LARGE_FONT);
   GUI_DispStringCenter(loc.x, loc.y, liveicon->lines[Levelindex].text);
   GUI_SetColor(WHITE);
-  GUI_DispStringCenter(loc.x-2, loc.y-2, liveicon->lines[Levelindex].text);
+  GUI_DispStringCenter(loc.x - 1, loc.y - 1, liveicon->lines[Levelindex].text);
   GUI_RestoreColorDefault();
 } //showTextOnIcon
 
