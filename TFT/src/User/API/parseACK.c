221bd8e6512d18c3a8d2b548eb25857626ed365a
ochkariky <63300924+ochkariky@users.noreply.github.com>
2021-03-25 03:09:49 +0300

    resolve conflict2
    minor change

--- TFT/src/User/API/parseACK.c
+++ TFT/src/User/API/parseACK.c
@@ -476,7 +476,7 @@ void parseACK(void)
         speedSetCurPercent(1, ack_value());
         speedQuerySetWait(false);
       }
-      // parse and store fan speed
+      // parse and store M106, fan speed
       else if (ack_seen("M106 P"))
       {
         uint8_t i = ack_value();
