c4439452cf3e5857fac66fe7812787eed4094667
Gurmeet Athwal <gurmeet.athwal@gmail.com>
2021-04-01 08:44:28 +0530

    Reduce RAM and memory usage & rename Macros.h to RRFMacros.h (#1769)

--- TFT/src/User/Menu/RRFMacros.h
+++ TFT/src/User/Menu/RRFMacros.h
@@ -1,13 +1,11 @@
-#ifndef _MACROS_H_
-#define _MACROS_H_
+#ifndef _RRFMACROS_H_
+#define _RRFMACROS_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void menuCallMacro(void);
-void macroListDraw(void);
-
 #ifdef __cplusplus
 }
 #endif
