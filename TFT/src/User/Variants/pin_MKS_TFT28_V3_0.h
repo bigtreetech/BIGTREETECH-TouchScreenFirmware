2dad4466158d90bdce9d6c9b6e2ba0bfff822b00
digant73 <diguardo@tiscali.it>
2021-04-08 16:06:55 +0200

    renamed all levelcorner.bmp to level_corner.bmp

---
+++ TFT/src/User/Variants/pin_MKS_TFT28_V3_0.h
@@ -0,0 +1,11 @@
+#ifndef _PIN_MKS_TFT28_V3_0_H_ // modify to actual filename !!!
+#define _PIN_MKS_TFT28_V3_0_H_ // modify to actual filename !!!
+
+// Hardware version config
+#ifndef HARDWARE_VERSION
+  #define HARDWARE_VERSION "TFT28_V3.0"
+#endif
+
+#include "pin_MKS_TFT32_V1_3.h"
+
+#endif
