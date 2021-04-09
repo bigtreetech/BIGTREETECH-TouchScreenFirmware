d727ead0cedfb31f747f7ec7168e54ae0fb63f4d
digant73 <diguardo@tiscali.it>
2021-04-09 21:01:46 +0200

    cleanup on MKS binary generation

--- buildroot/scripts/short_out_filename.py
+++ buildroot/scripts/short_out_filename.py
@@ -6,13 +6,10 @@ def make_short_file(source, target, env):
     buildDir = env.subst('$BUILD_DIR')
     build_flags = env.ParseFlags(env['BUILD_FLAGS'])
     flags = {k: v for (k, v) in build_flags.get("CPPDEFINES")}
-    if (flags.get("HARDWARE") == "MKS_TFT28_V3_0" or flags.get("HARDWARE") == "MKS_TFT28_V4_0"):
-        filename = "MKSTFT28"
-    else:
-        filename = flags.get("HARDWARE") + "." + flags.get("SOFTWARE_VERSION")
+    filename = flags.get("HARDWARE") + "." + flags.get("SOFTWARE_VERSION")
     filename_short = flags.get("HARDWARE_SHORT") + flags.get("SOFTWARE_VERSION_SHORT")
     # do some actions
-    shutil.copyfile(buildDir+"/"+filename+".bin", buildDir+"/"+filename_short+".new")
+    shutil.copyfile(buildDir + "/" + filename + ".bin", buildDir + "/" + filename_short + ".new")
     print("Done.")
 
 env.AddPostAction("buildprog", make_short_file)
