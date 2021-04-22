ddb60b9069f00ace07b459a77ea860bafea60eeb
guruathwal <gurmeet.athwal@gmail.com>
2021-04-22 23:24:04 +0530

    Fix wrong size of settings

--- TFT/src/User/SanityCheck.c
+++ TFT/src/User/SanityCheck.c
@@ -5,9 +5,9 @@ void sizecheck(void)
 {
   // if error shows 'the size of an array must be greater than zero' or 'size of unnamed array is negative'
   // then the size of the array is larger than allocated size in flash.
-  SIZE_CHECK((sizeof(SETTINGS) + 16) > PARA_SIZE);                   // Size of infoSettings is larger than allocated size in flash.
-  SIZE_CHECK(sizeof(STRINGS_STORE) > STRINGS_STORE_MAX_SIZE);        // Size of strings_store is larger than allocated size in flash.
-  SIZE_CHECK(sizeof(CUSTOM_GCODES) > CUSTOM_GCODE_MAX_SIZE);         // Size of custom_gcodes is larger than allocated size in flash.
-  SIZE_CHECK(sizeof(PRINT_GCODES) > PRINT_GCODES_MAX_SIZE);          // Size of print_gcodes is larger than allocated size in flash.
-  SIZE_CHECK((LABEL_NUM * MAX_LANG_LABEL_LENGTH) > LANGUAGE_SIZE);   // Size of Language pack is larger than allocated size in flash.
+  SIZE_CHECK((sizeof(SETTINGS) + 12 + sizeof(TSC_Para)) > PARA_SIZE); // Size of infoSettings is larger than allocated size in flash.
+  SIZE_CHECK(sizeof(STRINGS_STORE) > STRINGS_STORE_MAX_SIZE);         // Size of strings_store is larger than allocated size in flash.
+  SIZE_CHECK(sizeof(CUSTOM_GCODES) > CUSTOM_GCODE_MAX_SIZE);          // Size of custom_gcodes is larger than allocated size in flash.
+  SIZE_CHECK(sizeof(PRINT_GCODES) > PRINT_GCODES_MAX_SIZE);           // Size of print_gcodes is larger than allocated size in flash.
+  SIZE_CHECK((LABEL_NUM * MAX_LANG_LABEL_LENGTH) > LANGUAGE_SIZE);    // Size of Language pack is larger than allocated size in flash.
 }
