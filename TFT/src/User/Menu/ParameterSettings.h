11905cbbbe75d8eb54a300c19405afead6c2c456
guruathwal <gurmeet.athwal@gmail.com>
2021-05-03 12:40:32 +0530

    Update ParameterSettings.h

--- TFT/src/User/Menu/ParameterSettings.h
+++ TFT/src/User/Menu/ParameterSettings.h
@@ -8,15 +8,7 @@ extern "C" {
 #include "menu.h"
 #include "MachineParameters.h"
 
-// Global Info
-#define GLOBALICON_WIDTH    (BYTE_WIDTH * 2)
-#define GLOBALICON_HEIGHT   GLOBALICON_WIDTH
-#define GLOBALICON_INTERVAL 2
-
 void menuParameterSettings(void);
-
-void loopTemperatureStatus(void);
-int16_t drawTemperatureStatus(void);
 
 #ifdef __cplusplus
 }
