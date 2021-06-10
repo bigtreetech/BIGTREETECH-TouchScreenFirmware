5276254479934fc59dc2c611e8439e68bb66f72d
Gurmeet Athwal <gurmeet.athwal@gmail.com>
2021-06-10 06:40:44 +0530

    Fix text display on TFT43/TFT50 (#1985)

--- TFT/src/User/Menu/PrintingMenu.c
+++ TFT/src/User/Menu/PrintingMenu.c
@@ -382,7 +382,7 @@ void drawPrintInfo(void)
                  IconCharSelect(CHARICON_INFO));
   GUI_DispStringInRectEOL(rect_of_keySS[17].x0 + BYTE_HEIGHT + STATUS_MSG_TITLE_XOFFSET,
                           rect_of_keySS[17].y0 + STATUS_MSG_ICON_YOFFSET,
-                          rect_of_keySS[17].x1 - BYTE_HEIGHT + STATUS_MSG_TITLE_XOFFSET,
+                          rect_of_keySS[17].x1 - STATUS_MSG_TITLE_XOFFSET,
                           rect_of_keySS[17].y1 - STATUS_MSG_ICON_YOFFSET,
                           (uint8_t *)textSelect(LABEL_PRINT_FINISHED));
   GUI_SetColor(INFOMSG_COLOR);
