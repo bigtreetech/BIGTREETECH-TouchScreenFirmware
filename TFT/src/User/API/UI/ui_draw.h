1cbb956639c56f982500c60e1965eddf22cf1aa1
digant73 <diguardo@tiscali.it>
2021-06-11 15:26:43 +0200

    reviewed code style in ui_draw

--- TFT/src/User/API/UI/ui_draw.h
+++ TFT/src/User/API/UI/ui_draw.h
@@ -11,8 +11,8 @@ extern "C" {
 #include "GUI.h"
 
 #define SPACE_X          ((LCD_WIDTH - ICON_WIDTH * 4) / 4)
-#define START_X          (SPACE_X/2)
-#define SPACE_X_PER_ICON (LCD_WIDTH/4)
+#define START_X          (SPACE_X / 2)
+#define SPACE_X_PER_ICON (LCD_WIDTH / 4)
 
 #define SPACE_Y          ((LCD_HEIGHT - ICON_START_Y - ICON_HEIGHT * 2) / 2)
 
@@ -37,8 +37,8 @@ void ICON_ReadDisplay(uint16_t sx, uint16_t sy, uint8_t icon);
 void ICON_PrepareRead(uint16_t sx, uint16_t sy, uint8_t icon);
 void ICON_PrepareReadEnd(void);
 uint16_t ICON_ReadPixel(int16_t x, int16_t y);
-bool model_DirectDisplay(GUI_POINT pos, char *gcode);
-bool model_DecodeToFlash(char *gcode);
+bool model_DirectDisplay(GUI_POINT pos, char * gcode);
+bool model_DecodeToFlash(char * gcode);
 void IMAGE_ReadDisplay(uint16_t sx, uint16_t sy, uint32_t address);
 void SMALLICON_ReadDisplay(uint16_t sx, uint16_t sy, uint8_t icon);
 void ICON_PressedDisplay(uint16_t sx, uint16_t sy, uint8_t icon);
