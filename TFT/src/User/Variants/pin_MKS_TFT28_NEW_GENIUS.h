3c1b91b37e3dcab6304402f9902a4422aa5968bb
kisslorand <kisslorand666@yahoo.com>
2021-06-21 23:37:51 +0300

    Stage 1.1.1

--- TFT/src/User/Variants/pin_MKS_TFT28_NEW_GENIUS.h
+++ TFT/src/User/Variants/pin_MKS_TFT28_NEW_GENIUS.h
@@ -6,10 +6,8 @@
   #define HARDWARE_VERSION "TFT28_NEW_GENIUS"
 #endif
 
-#if !defined(ST7920_EMULATOR) || defined(SPI3_PIN_SMART_USAGE)
-  #ifndef FIL_RUNOUT_PIN
-    #define FIL_RUNOUT_PIN PB0
-  #endif
+#if (!defined(ST7920_EMULATOR) && defined(FIL_RUNOUT_PIN))
+  #define FIL_RUNOUT_PIN PB0
 #endif
   
 #include "pin_MKS_TFT32_V1_4.h"
