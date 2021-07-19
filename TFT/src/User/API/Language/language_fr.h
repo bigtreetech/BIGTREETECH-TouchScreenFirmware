fac6d193c6f35280f81141b1893f625ffb0a8714
BigTreeTech <38851044+bigtreetech@users.noreply.github.com>
2021-07-19 10:12:02 +0800

    restore language_fr

--- TFT/src/User/API/Language/language_fr.h
+++ TFT/src/User/API/Language/language_fr.h
@@ -351,7 +351,7 @@
     #define STRING_Z_ALIGN                "Z Align"
     #define STRING_MACROS                 "Macros"
     #define STRING_MESH_VALID             "Test Mesh"
-    #define STRING_CONNECT_PROBE          "Connect probe before starting the process. Make sure to disconnect it afterwards."
+    #define STRING_CONNECT_PROBE          "Connectez le palpeur avant de démarrer le processus. Veillez à le déconnecter ensuite."
     #define STRING_CALIBRATION            "Calibration"
 
 #endif
