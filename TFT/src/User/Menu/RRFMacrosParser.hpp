da1a3229766667af1a05b83fde2eb604f70a113a
Perry Nguyen <pfnguyen@hanhuy.com>
2021-07-30 16:27:52 -0700

    Re-format with 2-space indentation

--- TFT/src/User/Menu/RRFMacrosParser.hpp
+++ TFT/src/User/Menu/RRFMacrosParser.hpp
@@ -7,14 +7,16 @@
 #include "vfs.h"
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
-    typedef struct {
-        bool is_directory;
-        TCHAR *display_name;
-        TCHAR *file_name;
-    } MACRO_LIST_ITEM;
-    void parseMacroListResponse(char *data);
+  typedef struct
+  {
+    bool is_directory;
+    TCHAR *display_name;
+    TCHAR *file_name;
+  } MACRO_LIST_ITEM;
+  void parseMacroListResponse(char *data);
 #ifdef __cplusplus
 }
 #endif
@@ -23,38 +25,42 @@ extern "C" {
 #include "JsonStreamingParser.hpp"
 #include <string.h>
 
-static const char * EMPTY = "";
-static const char * FILES = "files";
-class RRFMacrosParser : public JsonListener {
+static const char *EMPTY = "";
+static const char *FILES = "files";
+class RRFMacrosParser : public JsonListener
+{
 
-    private:
-    const char * current_key = EMPTY;
-    bool in_array = false;
-    uint16_t macroCount = 0;
-    MACRO_LIST_ITEM macroList[FILE_NUM];
-    public:
-    inline void startDocument() { }
-    inline void startObject() { }
-    inline void endObject() { }
-    inline void whitespace(char c) { }
-    virtual void endDocument();
-    inline void startArray()
-    {
-        in_array = strcmp(FILES, current_key) == 0;
-    }
-    inline void endArray()
-    {
-        in_array = false;
-    }
+private:
+  const char *current_key = EMPTY;
+  bool in_array = false;
+  uint16_t macroCount = 0;
+  MACRO_LIST_ITEM macroList[FILE_NUM];
 
-    inline void key(const char * key) {
-        current_key = key;
-    }
-    inline void value(const char * value) {
-        if (in_array)
-            handle_value(value);
-    }
-    void handle_value(const char * value);
+public:
+  inline void startDocument() {}
+  inline void startObject() {}
+  inline void endObject() {}
+  inline void whitespace(char c) {}
+  virtual void endDocument();
+  inline void startArray()
+  {
+    in_array = strcmp(FILES, current_key) == 0;
+  }
+  inline void endArray()
+  {
+    in_array = false;
+  }
+
+  inline void key(const char *key)
+  {
+    current_key = key;
+  }
+  inline void value(const char *value)
+  {
+    if (in_array)
+      handle_value(value);
+  }
+  void handle_value(const char *value);
 };
 #endif
 
