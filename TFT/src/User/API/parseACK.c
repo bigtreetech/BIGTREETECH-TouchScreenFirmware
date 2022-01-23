683a003ecdb8af47bc51d135d26eadeda9540e65
kisslorand <kisslorand666@yahoo.com>
2022-01-23 16:17:41 +0200

    Stage 3.8 - evaluate faster messages from the MB

--- TFT/src/User/API/parseACK.c
+++ TFT/src/User/API/parseACK.c
@@ -113,62 +113,43 @@ bool syncL2CacheFromL1(uint8_t port)
 
 static bool ack_cmp(const char * str)
 {
-  uint16_t i;
-  for (i = 0; i < dmaL2Cache_len && str[i] != 0; i++)
-  {
-    if (str[i] != dmaL2Cache[i])
-      return false;
-  }
-  if (str[i] != 0)
-    return false;
-  return true;
+  if (strcmp(dmaL2Cache, str) == 0)
+    return true;
+
+  return false;
 }
 
 static bool ack_seen(const char * str)
-{
-  int16_t str_len = strlen(str);
-  int16_t max_len = dmaL2Cache_len - str_len;
+{ // searches an expression in the whole dmaL2Cache
+  uint16_t str_len = strlen(str);
+  char* pStr_tmp;
 
-  if (max_len < 0)  // if str is longer than data present in cache, no match can be found
+  if (str_len > dmaL2Cache_len)  // if str is longer than data present in cache, no match can be found
     return false;
 
-  uint16_t i;
-
-  for (ack_index = 0; ack_index <= max_len; ack_index++)
+  if ((pStr_tmp = strstr(dmaL2Cache, str)) != NULL)
   {
-    for (i = 0; i < str_len && str[i] == dmaL2Cache[ack_index + i]; i++)
-    {}
-    if (i == str_len)  // if end of str is reached, a match was found
-    {
-      ack_index += i;
-      return true;
-    }
+    ack_index = pStr_tmp - dmaL2Cache + str_len;
+    return true;
   }
+  
   return false;
 }
 
 static bool ack_continue_seen(const char * str)
-{ // unlike "ack_seen()", this retains "ack_index" if the searched string is not found
-  int16_t str_len = strlen(str);
-  int16_t max_len = dmaL2Cache_len - str_len;
+{// searches an expression in dmaL2Cache from where a previous successful search left off
+  uint16_t str_len = strlen(str);
+  char* pStr_tmp;
 
-  if (max_len < 0)  // if str is longer than data present in cache, no match can be found
+  if (str_len > strlen(&dmaL2Cache[ack_index]))  // if str is longer than data left in cache, no match can be found
     return false;
 
-  uint16_t ack_index_orig = ack_index;
-  uint16_t i;
-
-  for (; ack_index <= max_len; ack_index++)
+  if ((pStr_tmp = strstr(&dmaL2Cache[ack_index], str)) != NULL)
   {
-    for (i = 0; i < str_len && str[i] == dmaL2Cache[ack_index + i]; i++)
-    {}
-    if (i == str_len)  // if end of str is reached, a match was found
-    {
-      ack_index += i;
-      return true;
-    }
+    ack_index = pStr_tmp - dmaL2Cache + str_len;
+    return true;
   }
-  ack_index = ack_index_orig;
+  
   return false;
 }
 
