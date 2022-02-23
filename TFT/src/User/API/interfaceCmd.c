5cd7105280a3d1b92c1330d07dbc6561a211361a
Cyril <guilouz6@hotmail.com>
2022-02-23 16:04:23 +0100

    Add support for Delta Diagonal Rod Trim

--- TFT/src/User/API/interfaceCmd.c
+++ TFT/src/User/API/interfaceCmd.c
@@ -1084,6 +1084,9 @@ void sendQueueCmd(void)
           if (cmd_seen('S')) setParameter(P_DELTA_CONFIGURATION, 1, cmd_float());
           if (cmd_seen('R')) setParameter(P_DELTA_CONFIGURATION, 2, cmd_float());
           if (cmd_seen('L')) setParameter(P_DELTA_CONFIGURATION, 3, cmd_float());
+          if (cmd_seen('A')) setParameter(P_DELTA_DIAGONAL_ROD, AXIS_INDEX_X, cmd_float());
+          if (cmd_seen('B')) setParameter(P_DELTA_DIAGONAL_ROD, AXIS_INDEX_Y, cmd_float());
+          if (cmd_seen('C')) setParameter(P_DELTA_DIAGONAL_ROD, AXIS_INDEX_Z, cmd_float());
           if (cmd_seen('X')) setParameter(P_DELTA_TOWER_ANGLE, AXIS_INDEX_X, cmd_float());
           if (cmd_seen('Y')) setParameter(P_DELTA_TOWER_ANGLE, AXIS_INDEX_Y, cmd_float());
           if (cmd_seen('Z')) setParameter(P_DELTA_TOWER_ANGLE, AXIS_INDEX_Z, cmd_float());
