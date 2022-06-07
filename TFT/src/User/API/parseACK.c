8063636e9dcb5942171f2d87f49f7acb4e6473c6
kisslorand <kisslorand666@yahoo.com>
2022-06-07 23:46:22 +0300

    Stage 1 - Hotend MPC tune

--- TFT/src/User/API/parseACK.c
+++ TFT/src/User/API/parseACK.c
@@ -798,6 +798,14 @@ void parseACK(void)
       {
         pidUpdateStatus(false);
       }
+      // parse M306 tuning end message (interrupted or finished)
+      else if (ack_seen("MPC Autotune"))
+      {
+        if (ack_continue_seen("finished"))
+          setMpcTuningResult(FINISHED);
+        else if (ack_continue_seen("interrupted"))
+          setMpcTuningResult(INTERRUPTED);
+      }
       // parse and store M355, case light message
       else if (ack_seen("Case light:"))
       {
@@ -970,6 +978,19 @@ void parseACK(void)
       else if (ack_seen("M209 S"))
       {
         setParameter(P_AUTO_RETRACT, 0, ack_value());
+      }
+      else if (ack_seen("M306"))
+      {
+        if (ack_continue_seen("E"))
+        {
+          uint8_t index = ack_value();
+
+          if (ack_continue_seen("P"))
+            setMpcHeaterPower(index, ack_value());
+          
+          if (ack_continue_seen("H"))
+            setMpcFilHeatCapacity(index, ack_value());
+        }
       }
       // parse and store Delta configuration / Delta tower angle (M665) and Delta endstop adjustments (M666)
       //
--- TFT/src/User/Menu/Pid.c
+++ TFT/src/User/Menu/Pid.c
@@ -3,32 +3,15 @@
 
 //#define ENABLE_PID_STATUS_UPDATE_NOTIFICATION
 
-const MENUITEMS pidWaitItems = {
-  // title
-  LABEL_PID_TITLE,
-  // icon                          label
-  {
-    {ICON_NULL,                    LABEL_NULL},
-    {ICON_NULL,                    LABEL_NULL},
-    {ICON_NULL,                    LABEL_NULL},
-    {ICON_NULL,                    LABEL_NULL},
-    {ICON_NULL,                    LABEL_NULL},
-    {ICON_NULL,                    LABEL_NULL},
-    {ICON_NULL,                    LABEL_NULL},
-    {ICON_NULL,                    LABEL_NULL},
-  }
-};
-
 const char *const pidCmdMarlin[] = PID_CMD_MARLIN;
 const char *const pidCmdRRF[] = PID_CMD_RRF;
 static int16_t pidHeaterTarget[MAX_HEATER_COUNT] = {0};
 static uint8_t curTool_index = NOZZLE0;
 static uint8_t degreeSteps_index = 1;
-uint32_t pidTimeout = 0;
-uint8_t pidCounter = 0;
-bool pidSucceeded = false;
-bool pidRunning = false;
-bool pidInitialized = false;
+static uint32_t pidTimeout = 0;
+static uint8_t pidCounter = 0;
+static bool pidSucceeded = false;
+static bool pidRunning = false;
 
 // called by parseAck() to notify PID process status
 void pidUpdateStatus(bool succeeded)
@@ -126,37 +109,6 @@ static inline void pidUpdateCounter(void)
   }
 }
 
-void menuPidWait(void)
-{
-  bool isPressed = false;
-  bool isReleased = false;
-
-  menuDrawPage(&pidWaitItems);
-
-  GUI_DispString(20, BYTE_HEIGHT * 5, textSelect(LABEL_PID_START_INFO_2));
-  GUI_DispStringInRectEOL(20, BYTE_HEIGHT * 7, LCD_WIDTH - 20, LCD_HEIGHT, textSelect(LABEL_PID_START_INFO_3));
-
-  while (MENU_IS(menuPidWait))
-  {
-    if (!isPressed)  // if touch screen is not yet pressed
-    {
-      if (isPress())  // if touch screen is now pressed
-        isPressed = true;
-    }
-    else if (!isPress())  // if touch screen is now released
-    {
-      isReleased = true;
-    }
-
-    if (isReleased)
-      CLOSE_MENU();
-
-    pidCheckTimeout();
-
-    loopProcess();
-  }
-}
-
 static inline void pidStart(void)
 {
   const char *const *pidCmd = (infoMachineSettings.firmwareType == FW_REPRAPFW) ? pidCmdRRF : pidCmdMarlin;
@@ -169,12 +121,7 @@ static inline void pidStart(void)
   LED_SetEventColor(&ledRed, false);  // set (neopixel) LED light to RED
   LCD_SET_KNOB_LED_IDLE(false);       // set infoSettings.knob_led_idle temporary to OFF
 
-  if (infoMachineSettings.firmwareType != FW_REPRAPFW)
-    mustStoreCmd("M106 S255\n");  // set fan speed to max
-
-  mustStoreCmd("G4 S1\n");  // wait 1 sec
-
-  for (uint8_t i = 0; i < MAX_HEATER_COUNT; i++)  // hotends + bed + chamber
+  for (uint8_t i = 0; i < MAX_HEATER_PID_COUNT; i++)  // hotends + bed
   {
     if (pidHeaterTarget[i] > 0)
     {
@@ -182,10 +129,6 @@ static inline void pidStart(void)
       mustStoreCmd("G4 S1\n");                                       // wait 1 sec
     }
   }
-
-  mustStoreCmd("M107\n");  // stop fan
-
-  OPEN_MENU(menuPidWait);
 }
 
 void menuPid(void)
@@ -209,15 +152,8 @@ void menuPid(void)
 
   KEY_VALUES key_num = KEY_IDLE;
 
-  if (!pidInitialized)  // in order to keep the current set values even when reloading the menu
-  {                     // we initialize the data structure only the first time the menu is loaded
-    for (uint8_t i = NOZZLE0; i < MAX_HEATER_COUNT; i++)
-    {
-      pidHeaterTarget[i] = 0;
-    }
-
-    pidInitialized = true;
-  }
+  if (hasMPC() && curTool_index < MAX_HOTEND_COUNT)
+    curTool_index = MAX_HOTEND_COUNT;  // there's no PID for hotends if MPC enabled
 
   pidItems.items[KEY_ICON_4] = itemTool[curTool_index];
   pidItems.items[KEY_ICON_5] = itemDegreeSteps[degreeSteps_index];
@@ -227,67 +163,66 @@ void menuPid(void)
 
   while (MENU_IS(menuPid))
   {
-    key_num = menuKeyGetValue();
-
-    switch (key_num)
+    if (!pidRunning)
     {
-      case KEY_ICON_0:
-      case KEY_DECREASE:
-        if (pidHeaterTarget[curTool_index] > 0)
-          pidHeaterTarget[curTool_index] =
-              NOBEYOND(0, pidHeaterTarget[curTool_index] - degreeSteps[degreeSteps_index],
-                       infoSettings.max_temp[curTool_index]);
+      key_num = menuKeyGetValue();
 
-        temperatureReDraw(curTool_index, &pidHeaterTarget[curTool_index], true);
-        break;
-
-      case KEY_INFOBOX:
+      switch (key_num)
       {
-        int16_t val = editIntValue(0, infoSettings.max_temp[curTool_index], 0, pidHeaterTarget[curTool_index]);
+        case KEY_ICON_0:
+        case KEY_DECREASE:
+          if (pidHeaterTarget[curTool_index] > 0)
+            pidHeaterTarget[curTool_index] =
+                NOBEYOND(0, pidHeaterTarget[curTool_index] - degreeSteps[degreeSteps_index],
+                        infoSettings.max_temp[curTool_index]);
 
-        if (val != pidHeaterTarget[curTool_index])  // if value is different than target, change it
-          pidHeaterTarget[curTool_index] = val;
+          temperatureReDraw(curTool_index, &pidHeaterTarget[curTool_index], true);
+          break;
 
-        temperatureReDraw(curTool_index, &pidHeaterTarget[curTool_index], false);
-        break;
-      }
-
-      case KEY_ICON_3:
-      case KEY_INCREASE:
-        if (pidHeaterTarget[curTool_index] < infoSettings.max_temp[curTool_index])
-          pidHeaterTarget[curTool_index] =
-              NOBEYOND(0, pidHeaterTarget[curTool_index] + degreeSteps[degreeSteps_index],
-                       infoSettings.max_temp[curTool_index]);
-
-        temperatureReDraw(curTool_index, &pidHeaterTarget[curTool_index], true);
-        break;
-
-      case KEY_ICON_4:
-        do
+        case KEY_INFOBOX:
         {
-          curTool_index = (curTool_index + 1) % MAX_HEATER_COUNT;
-        } while (!heaterDisplayIsValid(curTool_index) || curTool_index == CHAMBER);
+          int16_t val = editIntValue(0, infoSettings.max_temp[curTool_index], 0, pidHeaterTarget[curTool_index]);
 
-        pidItems.items[key_num] = itemTool[curTool_index];
+          if (val != pidHeaterTarget[curTool_index])  // if value is different than target, change it
+            pidHeaterTarget[curTool_index] = val;
 
-        menuDrawItem(&pidItems.items[key_num], key_num);
-        temperatureReDraw(curTool_index, &pidHeaterTarget[curTool_index], false);
-        break;
-
-      case KEY_ICON_5:
-        degreeSteps_index = (degreeSteps_index + 1) % ITEM_DEGREE_NUM;
-        pidItems.items[key_num] = itemDegreeSteps[degreeSteps_index];
-
-        menuDrawItem(&pidItems.items[key_num], key_num);
-        break;
-
-      case KEY_ICON_6:
-        if (pidRunning)
-        {
-          addToast(DIALOG_TYPE_ERROR, (char *) textSelect(LABEL_PROCESS_RUNNING));
+          temperatureReDraw(curTool_index, &pidHeaterTarget[curTool_index], false);
+          break;
         }
-        else
-        {
+
+        case KEY_ICON_3:
+        case KEY_INCREASE:
+          if (pidHeaterTarget[curTool_index] < infoSettings.max_temp[curTool_index])
+            pidHeaterTarget[curTool_index] =
+                NOBEYOND(0, pidHeaterTarget[curTool_index] + degreeSteps[degreeSteps_index],
+                        infoSettings.max_temp[curTool_index]);
+
+          temperatureReDraw(curTool_index, &pidHeaterTarget[curTool_index], true);
+          break;
+
+        case KEY_ICON_4:
+          if (!hasMPC())
+          {
+            do
+            {
+              curTool_index = (curTool_index + 1) % MAX_HEATER_PID_COUNT;
+            } while (!heaterDisplayIsValid(curTool_index));
+
+            pidItems.items[key_num] = itemTool[curTool_index];
+
+            menuDrawItem(&pidItems.items[key_num], key_num);
+            temperatureReDraw(curTool_index, &pidHeaterTarget[curTool_index], false);
+            break;
+
+          case KEY_ICON_5:
+            degreeSteps_index = (degreeSteps_index + 1) % ITEM_DEGREE_NUM;
+            pidItems.items[key_num] = itemDegreeSteps[degreeSteps_index];
+
+            menuDrawItem(&pidItems.items[key_num], key_num);
+          }
+          break;
+
+        case KEY_ICON_6:
           pidUpdateCounter();
 
           if (pidCounter == 0)  // if no temperature was set to a value > 0
@@ -296,22 +231,27 @@ void menuPid(void)
           }
           else
           {
-            setDialogText(pidItems.title.index, LABEL_PID_START_INFO, LABEL_CONFIRM, LABEL_CANCEL);
+            setDialogText(pidItems.title.index, LABEL_TUNE_START_INFO, LABEL_CONFIRM, LABEL_CANCEL);
             showDialog(DIALOG_TYPE_QUESTION, pidStart, NULL, NULL);
           }
-        }
-        break;
+          break;
 
-      case KEY_ICON_7:
-        // set (neopixel) LED light to current color or to OFF according to infoSettings.led_always_on and
-        // restore infoSettings.knob_led_idle and knob LED color to their default values
-        LED_SetPostEventColor();
+        case KEY_ICON_7:
+          // set (neopixel) LED light to current color or to OFF according to infoSettings.led_always_on and
+          // restore infoSettings.knob_led_idle and knob LED color to their default values
+          LED_SetPostEventColor();
 
-        CLOSE_MENU();
-        break;
+          CLOSE_MENU();
+          break;
 
-      default:
-        break;
+        default:
+          break;
+      }
+    }
+    else if (getMenuType() != MENU_TYPE_SPLASH)
+    {
+      setDialogText(LABEL_SCREEN_INFO, LABEL_BUSY, LABEL_NULL, LABEL_NULL);
+      showDialog(DIALOG_TYPE_INFO, NULL, NULL, NULL);
     }
 
     pidCheckTimeout();
