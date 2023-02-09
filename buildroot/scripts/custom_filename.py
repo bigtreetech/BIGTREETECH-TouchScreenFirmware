6b250e25f9c746478907c2bd9636f2f63f35cb64
Gurmeet Athwal <gurmeet.athwal@gmail.com>
2023-02-09 11:47:13 +0530

    Add auto copying firmware script to removable storage and cleanup (#2673)
    - Add auto copying firmware script to removable storage/SD card
    - Make file path generation in scripts safer for cross platform usage.
    - Cleanup pre/post build scripts

--- buildroot/scripts/custom_filename.py
+++ buildroot/scripts/custom_filename.py
@@ -1,13 +1,13 @@
 Import("env")
 
-print("Generating firmware...")
 build_flags = env.ParseFlags(env['BUILD_FLAGS'])
 flags = {k: v for (k, v) in build_flags.get("CPPDEFINES")}
 filename = flags.get("BINARY_FILENAME")
+# set file name by hardware and firmware version
 if filename == None:
     filename = flags.get("HARDWARE") + "." + flags.get("SOFTWARE_VERSION")
+# rename firmware if portrait mode is selected
 if flags.get("PORTRAIT_MODE") != None:
     filename = filename + flags.get("PORTRAIT_MODE")
-print("Done.")
 
 env.Replace(PROGNAME = filename)
