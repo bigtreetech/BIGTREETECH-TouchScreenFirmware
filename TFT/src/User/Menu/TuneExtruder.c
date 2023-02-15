bd7efb4398405d09e1029eac3d96412f724aa876
digant73 <diguardo@tiscali.it>
2023-02-15 16:36:40 +0100

    minor update

--- TFT/src/User/Menu/TuneExtruder.c
+++ TFT/src/User/Menu/TuneExtruder.c
@@ -34,9 +34,9 @@ static inline void extrudeFilament(void)
 {
   // check and adopt current E-steps
   mustStoreCmd("M92\n");
-  infoParameters.StepsPerMM[E_AXIS] = 0;  // reset E-steps value
+  setParameter(P_STEPS_PER_MM, E_AXIS, 0.0f);  // reset E-steps value
 
-  while (infoParameters.StepsPerMM[E_AXIS] == 0)  // wait until E-steps is updated
+  while (getParameter(P_STEPS_PER_MM, E_AXIS) == 0.0f)  // wait until E-steps is updated
   {
     loopProcess();
   }
