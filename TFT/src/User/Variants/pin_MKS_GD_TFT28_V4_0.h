85536406dc3d339beba11d941dc750c96912f9e9
Ciotto <mail@ci8.it>
2023-03-08 18:42:34 +0100

    Add SERIAL_PORT_2 configuration

--- TFT/src/User/Variants/pin_MKS_GD_TFT28_V4_0.h
+++ TFT/src/User/Variants/pin_MKS_GD_TFT28_V4_0.h
@@ -28,12 +28,12 @@
 // SERIAL_PORT_X: communicating with other controllers (OctoPrint, ESP3D, other UART Touch Screen etc...)
 #ifndef SERIAL_PORT
   #define SERIAL_PORT   _USART2  // default USART port
-  #define SERIAL_PORT_2 _USART1
-  #define SERIAL_PORT_3 _USART3
+  #define SERIAL_PORT_2 _USART3
+  #define SERIAL_PORT_3 _USART1
   #define USART2_TX_PIN PD5
   #define USART2_RX_PIN PD6
-  // #define USART3_TX_PIN PD8
-  // #define USART3_RX_PIN PD9
+  #define USART3_TX_PIN PD8
+  #define USART3_RX_PIN PD9
   #define SERIAL_PORT_4 _UART4
 #endif
 
