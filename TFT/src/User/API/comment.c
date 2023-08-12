4511794ab93e45ac6ea6e65d9011bc4c4b7aad5a
digant73 <diguardo@tiscali.it>
2023-08-12 12:06:36 +0200

    minor cleanup

--- TFT/src/User/API/comment.c
+++ TFT/src/User/API/comment.c
@@ -12,26 +12,6 @@ void setTimeFromSlicer(bool present)
   slicerTimePresence = present;
 }
 
-char * getNextToken(char * str)
-{
-  char * token = strtok(str, TOKEN_DELIMITERS);
-
-  if (token != NULL)  // if a token was retrieved, convert the token string to lowercase
-    strlwr(token);
-
-  return token;
-}
-
-char * getNextTokenValue(uint32_t * token_value)
-{
-  char * token = strtok(NULL, TOKEN_DELIMITERS);
-
-  if (token != NULL)  // if a token was retrieved, convert the token string to value
-    *token_value = strtoul(token, NULL, 0);
-
-  return token;
-}
-
 void parseComment(void)
 {
   if (gCodeCommentLine[0] == '\0')
@@ -47,71 +27,78 @@ void parseComment(void)
     char * token;
     uint32_t token_value;
 
-    if ((token = getNextToken(gCodeCommentLine)) != NULL)
+    token = strlwr(strtok(gCodeCommentLine, TOKEN_DELIMITERS));
+
+    // check for "layer" keyword in comment (layer number or layer count)
+    if (strcmp(token, "layer") == 0)
     {
-      // check for "layer" keyword in comment (layer number or layer count)
-      if (strcmp(token, "layer") == 0)
+      if ((token = strtok(NULL, TOKEN_DELIMITERS)) != NULL)
       {
-        if ((token = getNextToken(NULL)) != NULL)
+        strlwr(token);
+
+        if (strcmp(token, "count") == 0)  // check if next word is "count"
         {
-          if (strcmp(token, "count") == 0)  // check if next word is "count"
-          {
-            if (getNextTokenValue(&token_value) != NULL)  // get the layer number
-            {
-              if (token_value != 0)
-                setPrintLayerCount(token_value);
-            }
-          }
-          else if (NUMERIC(token[0]))  // check if a number is found
+          if ((token = strtok(NULL, TOKEN_DELIMITERS)) != NULL)  // get the layer number
           {
             token_value = strtoul(token, NULL, 0);
 
-            // "token_value == 0" for object by object printing, when print goes to the next object
-            // if there is "layer 0" add an offset of 1 (avoiding using an offset variable)
-            setPrintLayerNumber(((token_value == 0) || (getPrintLayerNumber() == token_value)) ? token_value + 1 : token_value);
+            if (token_value != 0)
+              setPrintLayerCount(token_value);
           }
         }
-      }
-
-      // check for "time" keyword in comment to retrieve total or elapsed time, Cura specific
-      else if (strcmp(token, "time") == 0 && slicerTimePresence == false)  // check if first word is "time"
-      {
-        if ((token = getNextToken(NULL)) != NULL)
+        else if (NUMERIC(token[0]))  // check if a number is found
         {
-          if (strcmp(token, "elapsed") == 0 && getPrintExpectedTime() > 0)  // check if next word is "elapsed"
-          {
-            if (getNextTokenValue(&token_value) != NULL)  // get the elapsed time in seconds
-            {
-              setPrintRemainingTime(getPrintExpectedTime() - token_value);
-            }
-          }
-          else if (NUMERIC(token[0]))  // check if a number is found
-          {
-            token_value = strtoul(token, NULL, 0);  // get the time in seconds
+          token_value = strtoul(token, NULL, 0);
 
-            setPrintExpectedTime(token_value);
-            setPrintRemainingTime(token_value);
+          // "token_value == 0" for object by object printing, when print goes to the next object
+          // if there is "layer 0" add an offset of 1 (avoiding using an offset variable)
+          setPrintLayerNumber(((token_value == 0) || (getPrintLayerNumber() == token_value)) ? token_value + 1 : token_value);
+        }
+      }
+    }
+
+    // check for "time" keyword in comment to retrieve total or elapsed time, Cura specific
+    else if (strcmp(token, "time") == 0 && slicerTimePresence == false)  // check if first word is "time"
+    {
+      if ((token = strtok(NULL, TOKEN_DELIMITERS)) != NULL)
+      {
+        strlwr(token);
+
+        if (strcmp(token, "elapsed") == 0 && getPrintExpectedTime() > 0)  // check if next word is "elapsed"
+        {
+          if ((token = strtok(NULL, TOKEN_DELIMITERS)) != NULL)  // get the elapsed time in seconds
+          {
+            setPrintRemainingTime(getPrintExpectedTime() - strtoul(token, NULL, 0));
+          }
+        }
+        else if (NUMERIC(token[0]))  // check if a number is found
+        {
+          token_value = strtoul(token, NULL, 0);  // get the time in seconds
+
+          setPrintExpectedTime(token_value);
+          setPrintRemainingTime(token_value);
+
+          if (getPrintProgressSource() < PROG_TIME && infoSettings.prog_source == 1)
+            setPrintProgressSource(PROG_TIME);
+        }
+      }
+    }
+
+    // check for "remaining" keyword in comment to retrieve remaining time, IdeaMaker specific
+    else if (strcmp(token, "remaining") == 0 && slicerTimePresence == false)  // check if first word is "remaining"
+    {
+      if ((token = strtok(NULL, TOKEN_DELIMITERS)) != NULL)
+      {
+        strlwr(token);
+
+        if (strcmp(token, "time") == 0)  // check if next word is "time"
+        {
+          if ((token = strtok(NULL, TOKEN_DELIMITERS)) != NULL)  // get the remaining time in seconds
+          {
+            setPrintRemainingTime(strtoul(token, NULL, 0));
 
             if (getPrintProgressSource() < PROG_TIME && infoSettings.prog_source == 1)
               setPrintProgressSource(PROG_TIME);
-          }
-        }
-      }
-
-      // check for "remaining" keyword in comment to retrieve remaining time, IdeaMaker specific
-      else if (strcmp(token, "remaining") == 0 && slicerTimePresence == false)  // check if first word is "remaining"
-      {
-        if ((token = getNextToken(NULL)) != NULL)
-        {
-          if (strcmp(token, "time") == 0)  // check if next word is "time"
-          {
-            if (getNextTokenValue(&token_value) != NULL)  // get the remaining time in seconds
-            {
-              setPrintRemainingTime(token_value);
-
-              if (getPrintProgressSource() < PROG_TIME && infoSettings.prog_source == 1)
-                setPrintProgressSource(PROG_TIME);
-            }
           }
         }
       }
