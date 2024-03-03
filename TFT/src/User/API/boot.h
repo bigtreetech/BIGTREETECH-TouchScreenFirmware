b5cf0885cb3313ba8158326dac92d15f46e786eb
digant73 <diguardo@tiscali.it>
2024-03-03 11:37:45 +0100

    minor cleanup

--- TFT/src/User/API/boot.h
+++ TFT/src/User/API/boot.h
@@ -21,7 +21,7 @@ extern "C" {
   #define STRINGS_STORE_MAX_SIZE    0x1000  // label strings max size
   #define PREHEAT_STORE_MAX_SIZE    0x1000  // preheat setting max size
   #define PRINT_GCODES_MAX_SIZE     0x5000  // start/end/cancel gcodes max size
-  #define CUSTOM_GCODE_MAX_SIZE     0x5000  // custom gocdes max size
+  #define CUSTOM_GCODE_MAX_SIZE     0x5000  // custom gcodes max size
   #define ICON_MAX_SIZE             0x5000
   #define INFOBOX_MAX_SIZE          0xB000
   #define SMALL_ICON_MAX_SIZE       0x2000
